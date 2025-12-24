46b1afd09867a446715c177d31d2adb945d5562f
Django Merope <dmerope@gmail.com>
2025-12-24 17:49:57 +0900

    Event share modal

---
+++ src/lib/components/widgets/event/share/EventShareModal.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	const { eventId }: { eventId: string } = $props();
+	import { z } from '$lib/zero.svelte';
+	import { appState } from '$lib/state.svelte';
+	import { listActionCodes } from '$lib/zero/query/action_code/list';
+	import { readEvent } from '$lib/zero/query/event/read';
+	const actionCodes = $derived.by(() => {
+		return z.createQuery(
+			listActionCodes(appState.queryContext, {
+				organizationId: appState.organizationId,
+				referenceId: eventId
+			})
+		);
+	});
+	const event = $derived.by(() => {
+		return z.createQuery(
+			readEvent(appState.queryContext, {
+				eventId: eventId
+			})
+		);
+	});
+	import * as Tabs from '$lib/components/ui/tabs/index.js';
+	const actionCodeSignup = $derived.by(() => {
+		return actionCodes.data?.find((actionCode) => actionCode.type === 'event_signup');
+	});
+	const actionCodeAttended = $derived.by(() => {
+		return actionCodes.data?.find((actionCode) => actionCode.type === 'event_attended');
+	});
+
+	import EmbedCode from './EmbedCode.svelte';
+	import CheckIn from './CheckIn.svelte';
+	import ShareEvent from './ShareEvent.svelte';
+</script>
+
+<Tabs.Root value="share">
+	<Tabs.List>
+		<Tabs.Trigger value="share">Share</Tabs.Trigger>
+		<Tabs.Trigger value="embed">Embed</Tabs.Trigger>
+		<Tabs.Trigger value="checkin">Check-in</Tabs.Trigger>
+	</Tabs.List>
+	<Tabs.Content value="share">
+		{#if event.data && actionCodeSignup}
+			<ShareEvent event={event.data} actionCode={actionCodeSignup} />
+		{/if}
+	</Tabs.Content>
+	<Tabs.Content value="embed">
+		{#if event.data}
+			<EmbedCode event={event.data} />
+		{/if}
+	</Tabs.Content>
+	<Tabs.Content value="checkin">
+		{#if event.data && actionCodeAttended}
+			<CheckIn event={event.data} actionCode={actionCodeAttended} />
+		{/if}
+	</Tabs.Content>
+</Tabs.Root>
---
+++ src/lib/components/widgets/event/share/ShareEvent.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+	import { type ReadEventZero } from '$lib/schema/event';
+	import { type ReadActionCodeZero } from '$lib/schema/action-code';
+	let { event, actionCode }: { event: ReadEventZero; actionCode: ReadActionCodeZero } = $props();
+	import ChevronsUpDownIcon from '@lucide/svelte/icons/chevrons-up-down';
+	import * as Collapsible from '$lib/components/ui/collapsible/index.js';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+	import { generateWhatsAppQRCode } from '$lib/utils/qr';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
+	import DownloadIcon from '@lucide/svelte/icons/download';
+	import { appState } from '$lib/state.svelte';
+	import { env } from '$env/dynamic/public';
+	import * as InputGroup from '$lib/components/ui/input-group/index.js';
+	const whatsAppSignupLink = $derived.by(() => {
+		return `https://wa.me/${appState.activeOrganization.data?.settings.whatsApp.number || env.PUBLIC_DEFAULT_WHATSAPP_NUMBER}/?text=${encodeURIComponent(`Send to check in to ${event.title} [#${actionCode.id}] (do not edit this message)`)}`;
+	});
+	import { UseClipboard } from '$lib/hooks/use-clipboard.svelte';
+	const clipboard = new UseClipboard();
+	import CheckIcon from '@lucide/svelte/icons/check';
+	import CopyIcon from '@lucide/svelte/icons/copy';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { dev } from '$app/environment';
+	import { toast } from 'svelte-sonner';
+	const eventSignupPageLink = $derived.by(() => {
+		return `http${dev ? '' : 's'}://${appState.activeOrganization.data?.slug}.${env.PUBLIC_ROOT_DOMAIN}/events/${event.slug}`;
+	});
+</script>
+
+<div class="grid w-full max-w-md gap-4">
+	<div class="text-sm text-muted-foreground">Share this event with your audience</div>
+	<div class="space-y-2">
+		<Label>Event signup page link</Label>
+		<InputGroup.Root>
+			<InputGroup.Input value={eventSignupPageLink} readonly />
+			<InputGroup.Addon align="inline-end">
+				<InputGroup.Button
+					aria-label="Copy"
+					title="Copy"
+					size="icon-xs"
+					onclick={() => {
+						clipboard.copy(eventSignupPageLink);
+						toast.success(`Copied to clipboard`);
+					}}
+				>
+					{#if clipboard.copied}
+						<CheckIcon />
+					{:else}
+						<CopyIcon />
+					{/if}
+				</InputGroup.Button>
+			</InputGroup.Addon>
+		</InputGroup.Root>
+	</div>
+	<div class="space-y-2">
+		<Label>WhatsApp signup link</Label>
+		<InputGroup.Root>
+			<InputGroup.Input value={whatsAppSignupLink} readonly />
+			<InputGroup.Addon align="inline-end">
+				<InputGroup.Button
+					aria-label="Copy"
+					title="Copy"
+					size="icon-xs"
+					onclick={() => {
+						clipboard.copy(whatsAppSignupLink);
+						toast.success(`Copied to clipboard`);
+					}}
+				>
+					{#if clipboard.copied}
+						<CheckIcon />
+					{:else}
+						<CopyIcon />
+					{/if}
+				</InputGroup.Button>
+			</InputGroup.Addon>
+		</InputGroup.Root>
+	</div>
+	{@render qrCode()}
+</div>
+
+{#snippet qrCode()}
+	<Collapsible.Root class="space-y-2">
+		<div class="flex items-center justify-between space-x-4">
+			<h4 class="text-sm font-semibold">View WhatsApp signup QR code</h4>
+			<Collapsible.Trigger
+				class={buttonVariants({ variant: 'ghost', size: 'sm', class: 'w-9 p-0' })}
+			>
+				<ChevronsUpDownIcon />
+				<span class="sr-only">Toggle</span>
+			</Collapsible.Trigger>
+		</div>
+		<Collapsible.Content class="space-y-2">
+			{#await generateWhatsAppQRCode(whatsAppSignupLink)}
+				<div class="flex items-center justify-center">
+					<Skeleton class="aspect-square size-24" />
+				</div>
+			{:then qrCode}
+				<img src={qrCode} alt="WhatsApp checkin link" class="mx-auto" />
+				<Button
+					variant="outline"
+					class="w-full"
+					onclick={() => {
+						const link = document.createElement('a');
+						link.href = qrCode;
+						link.download = `event-${event.slug}-signup-qr-code.png`;
+						link.click();
+					}}
+				>
+					<DownloadIcon /> Download QR code</Button
+				>
+			{/await}
+		</Collapsible.Content>
+	</Collapsible.Root>
+{/snippet}
