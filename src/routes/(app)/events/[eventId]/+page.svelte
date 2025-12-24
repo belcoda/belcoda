648c0e8d9dff5baca901cdade65a9138685e0331
Django Merope <dmerope@gmail.com>
2025-12-24 17:48:32 +0900

    Use the new event action button with dropdown on the event page

--- src/routes/(app)/events/[eventId]/+page.svelte
+++ src/routes/(app)/events/[eventId]/+page.svelte
@@ -1,22 +1,17 @@
 <script lang="ts">
 	import ContentLayout from '$lib/components/layouts/app/ContentLayout.svelte';
-	import { Button } from '$lib/components/ui/button/index.js';
-	import EllipsisIcon from '@lucide/svelte/icons/ellipsis';
 	const { params } = $props();
 	import { readEvent } from '$lib/zero/query/event/read';
 	import { z } from '$lib/zero.svelte';
 	import { appState } from '$lib/state.svelte';
-	import { type ReadEventZero } from '$lib/schema/event';
 	const event = $derived.by(() => {
 		return z.createQuery(readEvent(appState.queryContext, { eventId: params.eventId }));
 	});
 	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
-	import * as Card from '$lib/components/ui/card/index.js';
-	import * as Table from '$lib/components/ui/table/index.js';
-
 	import Avatar from '$lib/components/widgets/avatar/Avatar.svelte';
 	import RenderEventDetails from '$lib/components/layouts/app/sidebars/events/RenderEventDetails.svelte';
 	import EventSignups from '$lib/components/widgets/event/EventSignups.svelte';
+	import EventActionButton from './EventActionButton.svelte';
 </script>
 
 <ContentLayout rootLink="/events" {header}>
@@ -53,7 +48,11 @@
 			{/if}
 		</div>
 		<div>
-			<Button variant="outline"><EllipsisIcon class="size-5" /></Button>
+			{#if event.data}
+				<EventActionButton event={event.data} />
+			{:else}
+				<Skeleton class="h-10 w-16 rounded-lg" />
+			{/if}
 		</div>
 	</div>
 {/snippet}
