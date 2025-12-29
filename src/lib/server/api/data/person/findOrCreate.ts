55dcbd9de4beb29ad6ab40eb5a1bdcbc40361695
Django Merope <dmerope@gmail.com>
2025-12-29 11:27:51 +0900

    Created data functions in /data directory

---
+++ src/lib/server/api/data/person/findOrCreate.ts
@@ -0,0 +1,91 @@
+import { db } from '$lib/server/db';
+import { person, personTeam } from '$lib/schema/drizzle';
+import { eq, or, and, isNull } from 'drizzle-orm';
+
+import { personActionHelper, type PersonActionHelper } from '$lib/schema/person';
+import {
+	personAddedFrom,
+	DEFAULT_SOCIAL_MEDIA,
+	type PersonAddedFrom
+} from '$lib/schema/person/meta';
+import type { Transaction } from '$lib/server/db/zeroDrizzle';
+
+import { v7 as uuidv7 } from 'uuid';
+import { parse } from 'valibot';
+
+export async function findOrCreatePerson({
+	personAction,
+	addedFrom,
+	tx,
+	organizationId,
+	teamId
+}: {
+	personAction: PersonActionHelper;
+	addedFrom: PersonAddedFrom;
+	organizationId: string;
+	teamId?: string;
+	tx: Transaction;
+}) {
+	const parsedActionHelper = parse(personActionHelper, personAction);
+	const parsedAddedFrom = parse(personAddedFrom, addedFrom);
+	const whereConditions = [];
+	if (personAction.emailAddress) {
+		whereConditions.push(eq(person.emailAddress, personAction.emailAddress));
+	}
+	if (personAction.phoneNumber) {
+		whereConditions.push(eq(person.phoneNumber, personAction.phoneNumber));
+	}
+
+	// there should only ever be one because there is a unique index against organizationId and phoneNumber/emailAddress
+	const [personRecord] = await tx.dbTransaction.wrappedTransaction
+		.select()
+		.from(person)
+		.where(
+			and(
+				isNull(person.deletedAt),
+				eq(person.organizationId, organizationId),
+				or(...whereConditions)
+			)
+		);
+
+	if (personRecord) {
+		//Todo: Maybe we want to update the person here with the provided info at personAction
+		return personRecord;
+	}
+	// create a new person
+	const input: typeof person.$inferInsert = {
+		...parsedActionHelper,
+		preferredLanguage: parsedActionHelper.preferredLanguage || 'en',
+		id: uuidv7(),
+		organizationId: organizationId,
+		addedFrom: parsedAddedFrom,
+		mostRecentActivityAt: new Date(),
+		createdAt: new Date(),
+		updatedAt: new Date(),
+		doNotContact: false,
+		socialMedia: DEFAULT_SOCIAL_MEDIA,
+		mostRecentActivityPreview: null,
+		deletedAt: null
+	};
+
+	const [insertedPerson] = await tx.dbTransaction.wrappedTransaction
+		.insert(person)
+		.values(input)
+		.returning();
+	if (!insertedPerson) {
+		throw new Error('Unable to create person');
+	}
+
+	//if teamId is provided, add the person to the team
+	if (teamId) {
+		await db.insert(personTeam).values({
+			personId: insertedPerson.id,
+			teamId: teamId,
+			organizationId: organizationId,
+			createdAt: new Date()
+		});
+	}
+
+	//return person;
+	return insertedPerson;
+}
