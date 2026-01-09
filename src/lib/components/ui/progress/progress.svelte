2c5a90eaf02b91f5261e3c2538749ddc545641f1
Kenneth Lebu <kenlebu@gmail.com>
2026-01-09 19:29:28 +0300

    add progress component

---
+++ src/lib/components/ui/progress/progress.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { Progress as ProgressPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils.js';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		value = null,
+		max = 100,
+		indicatorClass,
+		...restProps
+	}: ProgressPrimitive.RootProps & {
+		indicatorClass?: string;
+	} = $props();
+</script>
+
+<ProgressPrimitive.Root
+	bind:ref
+	{value}
+	{max}
+	class={cn('relative h-4 w-full overflow-hidden rounded-full bg-secondary', className)}
+	{...restProps}
+>
+	<div
+		class={cn('h-full w-full flex-1 bg-primary transition-all', indicatorClass)}
+		style="transform: translateX(-{100 - ((value ?? 0) * 100) / (max ?? 1)}%)"
+	></div>
+</ProgressPrimitive.Root>
