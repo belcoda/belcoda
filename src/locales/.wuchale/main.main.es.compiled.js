d7db4f548f8dc451dad01ccf97185610d6b98008
Django Merope <dmerope@gmail.com>
2026-01-25 23:06:24 +0900

    Locales

--- src/locales/.wuchale/main.main.es.compiled.js
+++ src/locales/.wuchale/main.main.es.compiled.js
@@ -1,5 +1,5 @@
 
-            export let c = ["Informacion basico","Dia y tiempo","Idioma","Idiomas","Update your preferred language:","This will update your language for all the pages in the application.","Settings","Email from signatures are the names and email addresses that are used to send emails on behalf\nof the organization. You can add as many as you want, but they must be attached to a\nnon-public email address that you control.","Email from signatures"]
+            export let c = ["Informacion basico","Dia y tiempo","Idioma","Idiomas","Update your preferred language:","This will update your language for all the pages in the application.","Email from signatures are the names and email addresses that are used to send emails on behalf\nof the organization. You can add as many as you want, but they must be attached to a\nnon-public email address that you control.","Email from signatures","Settings"]
             // only during dev, for HMR
             let latestVersion = -1
             // @ts-ignore
