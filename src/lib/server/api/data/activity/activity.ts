28a295850109a126cc6afecc149ab4dce5c15434
Django Merope <dmerope@gmail.com>
2026-01-25 23:02:42 +0900

    Activitiy data functions

---
+++ src/lib/server/api/data/activity/activity.ts
@@ -0,0 +1,39 @@
+import { getTransaction, type Transaction } from '$lib/server/db/zeroDrizzle';
+
+import { activity as activityTable } from '$lib/schema/drizzle';
+
+import { v7 as uuidv7 } from 'uuid';
+
+export async function createActivityWhatsAppMessageIncoming({
+	organizationId,
+	personId,
+	referenceId,
+	unread = true,
+	tx: defaultTx
+}: {
+	organizationId: string;
+	personId: string;
+	referenceId: string;
+	unread?: boolean;
+	tx?: Transaction;
+}) {
+	const tx = defaultTx || (await getTransaction());
+	const toInsert: typeof activityTable.$inferInsert = {
+		id: uuidv7(),
+		organizationId,
+		personId,
+		userId: null,
+		type: 'whatsapp_message_incoming',
+		referenceId,
+		unread,
+		createdAt: new Date()
+	};
+	const result = await tx.dbTransaction.wrappedTransaction
+		.insert(activityTable)
+		.values(toInsert)
+		.returning();
+	if (result.length === 0) {
+		throw new Error('Failed to create activity');
+	}
+	return result[0];
+}
