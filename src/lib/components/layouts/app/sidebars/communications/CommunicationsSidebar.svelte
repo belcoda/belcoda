52de9db10019a043eb8e9677c4d77e67fe80e299
Kenneth Lebu <kenlebu@gmail.com>
2026-01-26 11:55:33 +0300

    add communications sidebar

--- src/lib/components/layouts/app/sidebars/communications/CommunicationsSidebar.svelte
+++ src/lib/components/layouts/app/sidebars/communications/CommunicationsSidebar.svelte
@@ -9,15 +9,10 @@
 	import { type ReadPersonZero } from '$lib/schema/person';
 	import { formatShortTimestamp } from '$lib/utils/date';
 
-	import * as InputGroup from '$lib/components/ui/input-group/index.js';
-	import SearchIcon from '@lucide/svelte/icons/search';
-	import * as Tabs from '$lib/components/ui/tabs/index.js';
-	import EllipsisIcon from '@lucide/svelte/icons/ellipsis';
 	import { Button } from '$lib/components/ui/button/index.js';
-	const activeTab = 'border-indigo-500 text-indigo-600';
 
-	const isWhatsApp = $derived(page.url.pathname.startsWith('/communications/whatsapp'));
-	const isEmail = $derived(page.url.pathname.startsWith('/communications/email'));
+	const isDrafts = $derived(page.url.pathname.includes('/drafts'));
+	const isSent = $derived(page.url.pathname.includes('/sent'));
 </script>
 
 <Sidebar.Root
@@ -33,97 +28,29 @@
 				<div class="text-2xl font-bold text-foreground">Communications</div>
 				<ActionsMenu />
 			</div>
-			<div class="mb-4 flex flex-col gap-4">
-				<div class="border-b border-gray-200">
-					<nav aria-label="Tabs" class="-mb-px flex">
-						<a
-							class="w-1/2 border-b-2 px-1 pt-2 pb-4 text-center text-sm font-medium text-gray-500"
-							class:border-primary={isWhatsApp}
-							class:text-primary={isWhatsApp}
-							class:border-transparent={!isWhatsApp}
-							class:hover:border-gray-300={!isWhatsApp}
-							class:hover:text-gray-700={!isWhatsApp}
-							href="/communications/whatsapp">WhatsApp</a
-						>
-						<a
-							class:border-primary={isEmail}
-							class:text-primary={isEmail}
-							class:border-transparent={!isEmail}
-							class:hover:border-gray-300={!isEmail}
-							class:hover:text-gray-700={!isEmail}
-							href="/communications/email"
-							class="w-1/2 border-b-2 px-1 pt-2 pb-4 text-center text-sm font-medium text-gray-500"
-							>Email</a
-						>
-					</nav>
-				</div>
-				<div class="flex flex-col gap-4 px-4">
-					{#if isWhatsApp}
-						<Button href="/communications/whatsapp/new" class="w-full"
-							><span class="icon-[mage--whatsapp-filled] size-6"></span> Compose WhatsApp</Button
-						>
-					{/if}
-					{#if isEmail}
-						<Button href="/communications/email/new" class="w-full"
-							><span class="icon-[mdi--email] size-5"></span> Compose email</Button
-						>
-					{/if}
-					<div class="flex justify-between gap-2">
-						<Tabs.Root value="drafts">
-							<Tabs.List>
-								<Tabs.Trigger value="drafts">Drafts</Tabs.Trigger>
-								<Tabs.Trigger value="sent">Sent</Tabs.Trigger>
-							</Tabs.List>
-						</Tabs.Root>
-						<InputGroup.Root class="grow bg-background">
-							<InputGroup.Input placeholder="Search..." />
-							<InputGroup.Addon>
-								<SearchIcon />
-							</InputGroup.Addon>
-						</InputGroup.Root>
-					</div>
-				</div>
+			<div class="mb-4 px-4 pt-4">
+				<Button href="/communications/drafts/email/new" class="w-full" size="lg">
+					<span class="icon-[mdi--email] size-5"></span> Compose Email
+				</Button>
 			</div>
 		</Sidebar.Header>
 		<Sidebar.Content>
 			<Sidebar.Group>
-				<Sidebar.GroupLabel>WhatsApp</Sidebar.GroupLabel>
 				<Sidebar.GroupContent>
 					<Sidebar.Menu>
 						<Sidebar.MenuItem>
-							<Sidebar.MenuButton>
-								<span>Drafts</span>
-							</Sidebar.MenuButton>
-							<Sidebar.MenuAction>
-								{#snippet child({ props })}
-									<a href="/communications/whatsapp/drafts" {...props}>
-										<EllipsisIcon />
-										<span class="sr-only">All drafts</span>
-									</a>
-								{/snippet}
-							</Sidebar.MenuAction>
+							<a href="/communications/drafts">
+								<Sidebar.MenuButton isActive={isDrafts}>
+									<span>Drafts</span>
+								</Sidebar.MenuButton>
+							</a>
 						</Sidebar.MenuItem>
 						<Sidebar.MenuItem>
-							<Sidebar.MenuButton>
-								<span>Sent</span>
-							</Sidebar.MenuButton>
-						</Sidebar.MenuItem>
-					</Sidebar.Menu>
-				</Sidebar.GroupContent>
-			</Sidebar.Group>
-			<Sidebar.Group>
-				<Sidebar.GroupLabel>Email</Sidebar.GroupLabel>
-				<Sidebar.GroupContent>
-					<Sidebar.Menu>
-						<Sidebar.MenuItem>
-							<Sidebar.MenuButton>
-								<span>Drafts</span>
-							</Sidebar.MenuButton>
-						</Sidebar.MenuItem>
-						<Sidebar.MenuItem>
-							<Sidebar.MenuButton>
-								<span>Sent</span>
-							</Sidebar.MenuButton>
+							<a href="/communications/sent">
+								<Sidebar.MenuButton isActive={isSent}>
+									<span>Sent</span>
+								</Sidebar.MenuButton>
+							</a>
 						</Sidebar.MenuItem>
 					</Sidebar.Menu>
 				</Sidebar.GroupContent>
