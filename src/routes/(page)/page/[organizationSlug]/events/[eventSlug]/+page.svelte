833c902e2b2a93832471553cd9e5e80fffc76269
Django Merope <dmerope@gmail.com>
2026-01-28 21:00:42 +0900

    Take theme from url params and pass to components

--- src/routes/(page)/page/[organizationSlug]/events/[eventSlug]/+page.svelte
+++ src/routes/(page)/page/[organizationSlug]/events/[eventSlug]/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	const { data } = $props();
 	import RenderEventPage from '$lib/components/layouts/public/event/RenderEventPage.svelte';
+	import { page } from '$app/state';
+	const paramTheme = page.url.searchParams.get('theme') || 'default';
+	const themes = ['default', 'embed'];
+	const theme = themes.includes(paramTheme) ? (paramTheme as 'default' | 'embed') : 'default';
 </script>
 
 <RenderEventPage
 	event={data.event}
 	organization={data.organization}
+	{theme}
 	form={data.form}
 	whatsAppSignupLink={data.whatsAppSignupLink}
 />
