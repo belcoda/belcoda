f7d88f3d94d470291c9706a3d87b1f56cb5465dc
Django Merope <dmerope@gmail.com>
2026-02-03 23:47:25 +0900

    Remove layout

--- src/routes/(app)/communications/email/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import ContentLayout from '$lib/components/layouts/app/ContentLayout.svelte';
-	const { children } = $props();
-</script>
-
-<ContentLayout rootLink='/communications/email'>
-	{@render children?.()}
-</ContentLayout>
