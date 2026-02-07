9c0ce251c0dea4ef66e917acd046a0adc264b62a
Django Merope <dmerope@gmail.com>
2026-02-07 12:46:38 +0900

    Make sure that zero api routes (server and mutate) are properly authenticated

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -50,7 +50,6 @@ const handleRequest: Handle = async ({ event, resolve }) => {
 		event.url.pathname.startsWith('/logout') ||
 		event.url.pathname.startsWith('/api/docs') ||
 		event.url.pathname.startsWith('/verify-email') ||
-		event.url.pathname.startsWith('/api/utils/zero') ||
 		event.url.pathname.startsWith('/api/auth') || //this is for the better-auth api which handles its own authentication
 		event.url.pathname.startsWith('/webhooks') ||
 		event.url.pathname.startsWith('/sentry-example-page')
