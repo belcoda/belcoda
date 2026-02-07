b2d88a17ea125aed1233aa18aff8ff89d08c54de
Django Merope <dmerope@gmail.com>
2026-02-07 12:46:54 +0900

    Add checks to make sure that zero is properly initialized before anything else loads

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -4,6 +4,11 @@
 	import { createAppState, setAppState, getAppState } from '$lib/state.svelte';
 	// svelte-ignore state_referenced_locally
 	const { session, queryContext, organizations, defaultActiveOrganizationId } = data;
+
+	//initialize the zero instance with the user id and query context
+	// IMPORTANT: this must be done before the appState is created, because the appState relies on the zero instance
+	import { zero } from '$lib/zero.svelte';
+	zero.init(session.user.id, queryContext);
 	const appStateInstance = createAppState({
 		session,
 		initialQueryContext: queryContext,
@@ -14,7 +19,7 @@
 </script>
 
 <!-- This is an important check to ensure that we always have a valid userId and active organization Id. These will be used with confidence throughout the rest of the application interface -->
-{#if appState.user?.details.type === 'complete' && appState.activeOrganization?.details.type === 'complete' && appState.organizations?.details.type === 'complete'}
+{#if zero.instance && appState.user?.details.type === 'complete' && appState.activeOrganization?.details.type === 'complete' && appState.organizations?.details.type === 'complete'}
 	{@render children()}
 {:else}
 	<div class="flex h-screen w-screen items-center justify-center">
