cb79b134af910f46c23291a63067de6491d799ea
Django Merope <dmerope@gmail.com>
2026-02-10 14:01:50 +0900

    Update push server implementation for mutators

--- src/routes/(api)/api/utils/zero/push/+server.ts
+++ src/routes/(api)/api/utils/zero/push/+server.ts
@@ -1,9 +1,10 @@
 import { json } from '@sveltejs/kit';
 import { error } from '@sveltejs/kit';
-import { processor } from '$lib/server/db/zeroPushProcessor';
-import { createMutators } from '$lib/server/api/mutate/server_mutators';
+import { handleMutateRequest } from '@rocicorp/zero/server';
+import { mustGetMutator } from '@rocicorp/zero';
+import { db as dbProvider } from '$lib/server/db/index';
 
-import { type MutatorAsyncTasks, type MutatorResult } from '$lib/zero/schema';
+import { mutators } from '$lib/server/api/mutate/server_mutators';
 import pino from '$lib/pino';
 import { getQueryContext } from '$lib/server/api/utils/auth/permissions.js';
 const log = pino(import.meta.url);
@@ -15,25 +16,23 @@ export async function POST(event) {
 			{ status: 401 }
 		);
 	}
-	const queryContext = await getQueryContext(event.locals.session.user.id);
-
 	try {
-		const asyncTasks: MutatorAsyncTasks = [];
-		const result: MutatorResult = []; // in the push processor, we ignore the outcome because we won't be returning data to the client
-		const processorOutput = await processor.process(
-			createMutators({ queryContext, asyncTasks, result }),
+		const queryContext = await getQueryContext(event.locals.session.user.id);
+		const result = await handleMutateRequest(
+			dbProvider,
+			(transact) =>
+				transact((tx, name, args) => {
+					const mutator = mustGetMutator(mutators, name);
+					return mutator.fn({
+						args,
+						tx,
+						ctx: queryContext
+					});
+				}),
 			event.request
 		);
 
-		const promiseResults = await Promise.allSettled(asyncTasks.map((task) => task()));
-		if (promiseResults.some((result) => result.status === 'rejected')) {
-			log.error(
-				promiseResults.find((result) => result.status === 'rejected')?.reason,
-				'Error executing async tasks in push request'
-			);
-			return error(500, 'Internal server error');
-		}
-		return json(processorOutput);
+		return json(result);
 	} catch (err) {
 		log.fatal(err, 'Error processing push protocol in push request');
 		return error(500, 'Internal server error');
