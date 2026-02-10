495aeef51d397176c5bb06ea221e0eee3f7a376d
Django Merope <dmerope@gmail.com>
2026-02-10 10:58:20 +0900

    Move mutators to use data functions

--- src/lib/server/api/mutate/person_note.ts
+++ src/lib/server/api/mutate/person_note.ts
@@ -1,115 +1,39 @@
-import type { Transaction } from '$lib/server/db/zeroDrizzle';
-import { type MutatorParams } from '$lib/zero/schema';
-
-import { personNote } from '$lib/schema/drizzle';
-import { eq, and } from 'drizzle-orm';
-import { personReadPermissions } from '$lib/zero/query/person/permissions';
-import { personNoteReadPermissions } from '$lib/zero/query/person_note/permissions';
+import { defineMutator } from '@rocicorp/zero';
 
 import {
-	type CreateMutatorSchemaZero,
-	type UpdateMutatorSchemaZero,
-	type DeleteMutatorSchemaZero
+	createMutatorSchemaZero,
+	updateMutatorSchemaZero,
+	deleteMutatorSchemaZero
 } from '$lib/schema/person-note';
 
-import { getQueue } from '$lib/server/queue';
+import * as dataFunctions from '$lib/server/api/data/person/note';
 
-export function createPersonNote(params: MutatorParams) {
-	return async function (tx: Transaction, args: CreateMutatorSchemaZero) {
-		const personRecord = await tx.query.person
-			.where('organizationId', '=', args.metadata.organizationId)
-			.where('deletedAt', 'IS', null)
-			.where('id', '=', args.metadata.personId)
-			.where((expr) => personReadPermissions(expr, params.queryContext))
-			.one()
-			.run();
-		if (!personRecord) {
-			throw new Error('Person not found');
+export const createPersonNote = defineMutator(
+	createMutatorSchemaZero,
+	async ({ tx, args, ctx }) => {
+		if (tx.location !== 'server') {
+			throw new Error('createPersonNote can only be called from the server');
 		}
+		await dataFunctions.createPersonNote({ tx, ctx, args });
+	}
+);
 
-		const personNoteToCreate: typeof personNote.$inferInsert = {
-			id: args.metadata.personNoteId,
-			organizationId: args.metadata.organizationId,
-			personId: args.metadata.personId,
-			note: args.input.note,
-			userId: params.queryContext.userId,
-			createdAt: new Date(),
-			updatedAt: new Date()
-		};
-
-		const [result] = await tx.dbTransaction.wrappedTransaction
-			.insert(personNote)
-			.values(personNoteToCreate)
-			.returning();
-		if (!result) {
-			throw new Error('Unable to create person note');
+export const updatePersonNote = defineMutator(
+	updateMutatorSchemaZero,
+	async ({ tx, args, ctx }) => {
+		if (tx.location !== 'server') {
+			throw new Error('updatePersonNote can only be called from the server');
 		}
+		await dataFunctions.updatePersonNote({ tx, ctx, args });
+	}
+);
 
-		params.result?.push(result);
-
-		params.asyncTasks.push(async () => {
-			const queue = await getQueue();
-			queue.insertActivity({
-				organizationId: args.metadata.organizationId,
-				personId: result.personId,
-				userId: params.queryContext.userId,
-				type: 'note_added',
-				referenceId: args.metadata.personNoteId,
-				unread: false
-			});
-		});
-	};
-}
-
-export function updatePersonNote(params: MutatorParams) {
-	return async function (tx: Transaction, args: UpdateMutatorSchemaZero) {
-		const personNoteRecord = await tx.query.personNote
-			.where('id', '=', args.metadata.personNoteId)
-			.where('organizationId', '=', args.metadata.organizationId)
-			.where((expr) => personNoteReadPermissions(expr, params.queryContext))
-			.one()
-			.run();
-		if (!personNoteRecord) {
-			throw new Error('Person note not found');
+export const deletePersonNote = defineMutator(
+	deleteMutatorSchemaZero,
+	async ({ tx, args, ctx }) => {
+		if (tx.location !== 'server') {
+			throw new Error('deletePersonNote can only be called from the server');
 		}
-
-		const [result] = await tx.dbTransaction.wrappedTransaction
-			.update(personNote)
-			.set({
-				note: args.input.note,
-				updatedAt: new Date()
-			})
-			.where(eq(personNote.id, args.metadata.personNoteId))
-			.returning();
-		if (!result) {
-			throw new Error('Unable to update person note');
-		}
-
-		params.result?.push(result);
-	};
-}
-
-export function deletePersonNote(params: MutatorParams) {
-	return async function (tx: Transaction, args: DeleteMutatorSchemaZero) {
-		const personNoteRecord = await tx.query.personNote
-			.where('id', '=', args.metadata.personNoteId)
-			.where('organizationId', '=', args.metadata.organizationId)
-			.where((expr) => personNoteReadPermissions(expr, params.queryContext))
-			.one()
-			.run();
-		if (!personNoteRecord) {
-			throw new Error('Person note not found');
-		}
-
-		const [result] = await tx.dbTransaction.wrappedTransaction
-			.update(personNote)
-			.set({ deletedAt: new Date() })
-			.where(eq(personNote.id, args.metadata.personNoteId))
-			.returning();
-		if (!result) {
-			throw new Error('Unable to delete person note');
-		}
-
-		params.result?.push(result);
-	};
-}
+		await dataFunctions.deletePersonNote({ tx, ctx, args });
+	}
+);
