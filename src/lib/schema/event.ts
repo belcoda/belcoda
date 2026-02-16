e8858059f5571dd49dc317a9c1e0f4bf5b761001
Django Merope <dmerope@gmail.com>
2026-02-16 13:02:51 +0900

    Move event theme to a separate schema and type in event validation schema file

--- src/lib/schema/event.ts
+++ src/lib/schema/event.ts
@@ -174,3 +174,7 @@ export const updateEventZeroMutatorSchema = v.object({
 });
 export type UpdateEventZeroMutatorSchema = v.InferInput<typeof updateEventZeroMutatorSchema>;
 export type UpdateEventZeroMutatorSchemaOutput = v.InferOutput<typeof updateEventZeroMutatorSchema>;
+
+export const eventThemes = ['default', 'embed'] as const;
+export const eventThemeSchema = v.picklist(eventThemes);
+export type EventTheme = v.InferOutput<typeof eventThemeSchema>;
--- src/routes/(page)/page/[organizationSlug]/events/[eventSlug]/signed-up/+page.svelte
+++ src/routes/(page)/page/[organizationSlug]/events/[eventSlug]/signed-up/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	const { data } = $props();
+	import { page } from '$app/state';
+	const paramTheme = page.url.searchParams.get('theme') || 'default';
+	const themes = ['default', 'embed'];
+	const theme = themes.includes(paramTheme) ? (paramTheme as 'default' | 'embed') : 'default';
+
 	import RenderEventPage from '$lib/components/layouts/public/event/RenderEventPage.svelte';
 </script>
 
-<RenderEventPage event={data.event} organization={data.organization} theme="default" />
+<RenderEventPage event={data.event} organization={data.organization} {theme} />
