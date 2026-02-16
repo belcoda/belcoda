25b6456b790d59128d0783692ac2b1c7a0212e66
Django Merope <dmerope@gmail.com>
2026-02-16 11:48:27 +0900

    Add a nbew route for svelteflow testing

---
+++ src/routes/(app)/communications/whatsapp/test/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import TestFlow from '$lib/components/flow/test/TestFlow.svelte';
+	import ContentLayout from '$lib/components/layouts/app/ContentLayout.svelte';
+</script>
+
+<ContentLayout rootLink="/communications">
+	{#snippet header()}
+		<h1>Test Flow</h1>
+	{/snippet}
+	{#snippet footer()}
+		<p>Footer</p>
+	{/snippet}
+	<TestFlow />
+</ContentLayout>
