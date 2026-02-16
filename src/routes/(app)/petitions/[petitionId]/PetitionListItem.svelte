6768a0d72ed54b07965036ab7aba0f532d570f2a
Kenneth Lebu <kenlebu@gmail.com>
2026-02-16 10:08:35 +0300

    move list item into it's own component

---
+++ src/routes/(app)/petitions/[petitionId]/PetitionListItem.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { t } from '$lib/index.svelte';
+	import Avatar from '$lib/components/widgets/avatar/Avatar.svelte';
+	import ColorBadge from '$lib/components/ui/colorbadge/badge.svelte';
+    let {petition}: {petition: Petition} = $props();
+</script>
+
+<div class="flex w-full items-center justify-start gap-3">
+    <div class="w-12">
+        <Avatar
+            src={petition.featureImage}
+            name1={petition.title}
+            class="size-12 rounded-lg"
+            imageClass="rounded-lg object-cover"
+        />
+    </div>
+    <div>
+        <div class="flex items-center gap-2">
+            <div class="text-lg font-medium">{petition.title}</div>
+            <ColorBadge color={petition.published ? 'green' : 'gray'}>
+                {petition.published ? t`Published` : t`Draft`}
+            </ColorBadge>
+        </div>
+        {#if petition.petitionTarget}
+            <div class="text-sm text-muted-foreground">
+                {t`Target:`}
+                {petition.petitionTarget}
+            </div>
+        {/if}
+    </div>
+</div>
