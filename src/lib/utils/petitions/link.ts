9225fbe1ff341b30cdff5a5fc6cc6aa78351a9f6
Kenneth Lebu <kenlebu@gmail.com>
2026-02-17 16:56:49 +0300

    helper function to generate the wa.me link

--- src/lib/utils/petitions/link.ts
+++ src/lib/utils/petitions/link.ts
@@ -10,3 +10,15 @@ export function getPetitionLink({
 }) {
 	return `http${dev ? '' : 's'}://${organizationSlug}.${env.PUBLIC_ROOT_DOMAIN}/petitions/${petitionSlug}`;
 }
+
+export function generateWhatsAppPetitionLink({
+	petitionTitle,
+	whatsAppNumber,
+	actionCode
+}: {
+	petitionTitle: string;
+	whatsAppNumber?: string | null;
+	actionCode: string;
+}) {
+	return `https://wa.me/${whatsAppNumber || env.PUBLIC_DEFAULT_WHATSAPP_NUMBER}/?text=${encodeURIComponent(`Sign petition for ${petitionTitle} #[${actionCode}] (do not edit this message)`)}`;
+}
