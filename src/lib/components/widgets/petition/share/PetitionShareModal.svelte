8eb2f2ce8ec3bbb887b1415fcdea489f97035409
Kenneth Lebu <kenlebu@gmail.com>
2026-02-17 17:25:15 +0300

    Add whatsapp details to the share petition modal

--- src/lib/components/widgets/petition/share/PetitionShareModal.svelte
+++ src/lib/components/widgets/petition/share/PetitionShareModal.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
 	const { petitionId }: { petitionId: string } = $props();
 	import { z } from '$lib/zero.svelte';
+	import { appState } from '$lib/state.svelte';
 	import queries from '$lib/zero/query/index';
 	import { t } from '$lib/index.svelte';
+	const actionCodes = $derived.by(() => {
+		return z.createQuery(
+			queries.actionCode.list({
+				organizationId: appState.organizationId,
+				referenceId: petitionId
+			})
+		);
+	});
 	const petition = $derived.by(() => {
 		return z.createQuery(queries.petition.read({ petitionId: petitionId }));
 	});
 	import * as Tabs from '$lib/components/ui/tabs/index.js';
+	const actionCodeSigned = $derived.by(() => {
+		return actionCodes.data?.find((actionCode) => actionCode.type === 'petition_signed');
+	});
 
 	import EmbedCode from './EmbedCode.svelte';
 	import CheckIn from './CheckIn.svelte';
@@ -20,8 +32,8 @@
 		<Tabs.Trigger value="checkin">{t`Check-in`}</Tabs.Trigger>
 	</Tabs.List>
 	<Tabs.Content value="share">
-		{#if petition.data}
-			<SharePetition petition={petition.data} />
+		{#if petition.data && actionCodeSigned}
+			<SharePetition petition={petition.data} actionCode={actionCodeSigned} />
 		{/if}
 	</Tabs.Content>
 	<Tabs.Content value="embed">
