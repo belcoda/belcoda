35441ddd935d0c1fa0a5b8d1537755bd08f2d6ad
Django Merope <dmerope@gmail.com>
2026-02-17 11:23:06 +0900

    Update community sidebar action button to be new person
    Previously it had a placeholder dropdown with a bunch of broken links.

--- src/locales/.wuchale/main.main.en.compiled.js
+++ src/locales/.wuchale/main.main.en.compiled.js
@@ -659,3 +659,15 @@ export let c = [
 	'Credit balance',
 	'API keys'
 ];
+// only during dev, for HMR
+let latestVersion = -1;
+// @ts-ignore
+export function update({ version, data }) {
+	if (latestVersion >= version) {
+		return;
+	}
+	for (const [index, item] of data['en'] ?? []) {
+		c[index] = item;
+	}
+	latestVersion = version;
+}
