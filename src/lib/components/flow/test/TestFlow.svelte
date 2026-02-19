6ace4774d177a2f2f1a651838f7902cf2ea7570f
Django Merope <dmerope@gmail.com>
2026-02-19 22:45:33 +0900

    Add flow panel

--- src/lib/components/flow/test/TestFlow.svelte
+++ src/lib/components/flow/test/TestFlow.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { SvelteFlow, Background } from '@xyflow/svelte';
+	import { SvelteFlow, Background, Panel } from '@xyflow/svelte';
 
 	import '@xyflow/svelte/dist/style.css';
 	import type { WhatsAppNodeData } from './types';
@@ -28,10 +28,32 @@
 	let edges = $state.raw([]);
 	import Message from './nodes/Message.svelte';
 	const nodeTypes = { message: Message };
+	import { Button } from '$lib/components/ui/button/index.js';
+	import PlusIcon from '@lucide/svelte/icons/plus';
 </script>
 
 <div class="h-full w-full">
 	<SvelteFlow bind:nodes bind:edges fitView {nodeTypes}>
+		<Panel position="top-left">
+			<Button
+				variant="outline"
+				size="sm"
+				onclick={() => {
+					nodes = [
+						...nodes,
+						{
+							id: 'node-' + nodes.length + 1,
+							type: 'message',
+							position: { x: 0, y: 0 },
+							data: { text: 'some text', buttons: [], imageUrl: null }
+						}
+					];
+				}}
+			>
+				<PlusIcon />
+				Add Node
+			</Button>
+		</Panel>
 		<Background />
 	</SvelteFlow>
 </div>
