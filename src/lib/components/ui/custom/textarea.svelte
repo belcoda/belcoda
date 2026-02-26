2e9451ced83932c782d24e398d0661a0994b64ea
Django Merope <dmerope@gmail.com>
2026-02-26 15:18:24 +0900

    Create custom proxy components

---
+++ src/lib/components/ui/custom/textarea.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { ComponentProps } from 'svelte';
+	import { Textarea } from '$lib/components/ui/textarea/index.js';
+
+	let {
+		ref = $bindable(null),
+		value = $bindable(),
+		class: className,
+		fallbackValue = undefined,
+		...restProps
+	}: ComponentProps<typeof Textarea> & { fallbackValue?: undefined | null } = $props();
+	function getValue() {
+		if (typeof value === 'string') {
+			return value;
+		} else {
+			return '';
+		}
+	}
+	function setValue(newValue: string) {
+		if (newValue === '') {
+			value = fallbackValue;
+		} else {
+			value = newValue;
+		}
+	}
+</script>
+
+<Textarea {ref} bind:value={getValue, setValue} class={className} {...restProps} />
