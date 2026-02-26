9008211dd36a8d6bd7ca16f4dddc72920601f4e2
Django Merope <dmerope@gmail.com>
2026-02-26 14:12:01 +0900

    test flow page

--- src/routes/(app)/communications/whatsapp/test/+page.svelte
+++ src/routes/(app)/communications/whatsapp/test/+page.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import TestFlow from '$lib/components/flow/test/TestFlow.svelte';
 	import ContentLayout from '$lib/components/layouts/app/ContentLayout.svelte';
+	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
 <ContentLayout rootLink="/communications">
 	{#snippet header()}
-		<h1>Test Flow</h1>
+		<h2 class="text-2xl font-bold">Local Organizers Mixer</h2>
 	{/snippet}
 	{#snippet footer()}
-		<p>Footer</p>
+		<div class="flex w-full justify-end gap-2">
+			<Button variant="outline">Cancel</Button>
+			<Button variant="default">Send</Button>
+		</div>
 	{/snippet}
 	<TestFlow />
 </ContentLayout>
