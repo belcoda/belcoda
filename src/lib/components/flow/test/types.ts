532ad1c23b3774ed0a9649910e63db5ca78ce09f
Django Merope <dmerope@gmail.com>
2026-02-26 14:11:39 +0900

    Types and schemas relating to flows

--- src/lib/components/flow/test/types.ts
+++ src/lib/components/flow/test/types.ts
@@ -1,17 +1,56 @@
+import { type XYPosition } from '@xyflow/svelte';
 export interface WhatsAppButton {
 	id: string;
 	label: string;
 }
-import { type XYPosition } from '@xyflow/svelte';
-export interface WhatsAppNodeData {
+
+export type NodeGenericData = {
 	id: string;
-	type: 'message';
+	type: string;
 	position: XYPosition;
 	width?: number | undefined;
 	height?: number | undefined;
+};
+
+export interface WhatsAppMessageData {
+	hideImage?: boolean;
+	text: string;
+	buttons: WhatsAppButton[];
+	imageUrl: string | null;
+}
+
+export interface WhatsAppNodeData extends NodeGenericData {
+	type: 'message';
 	data: {
+		hideImage?: boolean;
 		text: string;
 		buttons: WhatsAppButton[];
 		imageUrl: string | null;
 	};
 }
+
+export interface EventSignupNodeData extends NodeGenericData {
+	id: string;
+	type: 'eventSignup';
+	data: {
+		eventId: string;
+	};
+}
+
+export interface TagAddNodeData extends NodeGenericData {
+	id: string;
+	type: 'tagAdd';
+	data: {
+		tagId: string;
+	};
+}
+
+export interface TargetingNodeData extends NodeGenericData {
+	id: string;
+	type: 'targeting';
+	data: {
+		recipients: string[];
+	};
+}
+
+export type FlowNode = WhatsAppNodeData | EventSignupNodeData | TagAddNodeData | TargetingNodeData;
