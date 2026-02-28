c87307c40f73dede58dffe003f60c3bd40bf6644
Django Merope <dmerope@gmail.com>
2026-03-01 05:35:14 +0900

    Switch organization name/slug to use input group with modal for editing slug

--- src/routes/(auth)/organization/new/+page.svelte
+++ src/routes/(auth)/organization/new/+page.svelte
@@ -27,6 +27,14 @@
 	});
 	import * as Form from '$lib/components/ui/form/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
+	import * as InputGroup from '$lib/components/ui/input-group/index.js';
+	import { slugify } from '$lib/utils/slug';
+	import { useDebounce } from 'runed';
+	let editSlugOpen = $state(false);
+	import LinkIcon from '@lucide/svelte/icons/link';
+	import ResponsiveModal from '$lib/components/ui/responsive-modal/responsive-modal.svelte';
+	import { dev } from '$app/environment';
+	import { env } from '$env/dynamic/public';
 </script>
 
 <AuthLayout
@@ -44,26 +52,79 @@
 			{#if error}
 				<ErrorAlert>{error}</ErrorAlert>
 			{/if}
-			<Form.Field {form} name="name">
+			<Form.Field {form} name="name" class="w-full">
 				<Form.Control>
 					{#snippet children({ props })}
-						<Form.Label>{t`Name`}</Form.Label>
-						<Input {...props} bind:value={$data.name} autocomplete="organization" />
-						<Form.Description
-							>{t`The name of your organization. You cannot change it later.`}</Form.Description
-						>
-						<Form.FieldErrors />
+						<Form.Label>{t`Title`}</Form.Label>
+						<InputGroup.Root>
+							<InputGroup.Input
+								bind:value={$data.name}
+								{...props}
+								placeholder={t`Organization name`}
+								oninput={useDebounce(
+									() => {
+										if (!form.isTainted('slug')) {
+											data.update(
+												($store) => {
+													const slug = slugify($data.name ?? '');
+													return { ...$store, slug };
+												},
+												{ taint: false }
+											);
+										}
+									},
+									() => 300
+								)}
+							/>
+							{#if $data.name && $data.name.length > 0}
+								<InputGroup.Addon align="block-end" class="flex items-center justify-end gap-2">
+									<InputGroup.Text>
+										<LinkIcon class="size-4" /><span class="font-mono text-xs"
+											>http{dev ? '' : 's'}://${$data?.slug}.{env.PUBLIC_ROOT_DOMAIN}</span
+										>
+										<ResponsiveModal title={t`Edit organization slug`} bind:open={editSlugOpen}>
+											{#snippet trigger()}
+												<InputGroup.Button type="button">Edit</InputGroup.Button>
+											{/snippet}
+
+											<Form.Field {form} name="slug">
+												<Form.Control>
+													{#snippet children({ props })}
+														<Input
+															bind:value={$data.slug}
+															{...props}
+															class="font-mono"
+															placeholder={t`Slug`}
+														/>
+													{/snippet}
+												</Form.Control>
+												<Form.Description>
+													{t`This is a URL-friendly identifier for the organization that can be used in links for events, petitions and other pages.
+													It must be unique and can only contain lowercase letters, numbers, and
+													hyphens.`}
+												</Form.Description>
+												<Form.FieldErrors />
+											</Form.Field>
+											{#snippet footer()}
+												<Button
+													variant="default"
+													size="sm"
+													class="w-full"
+													type="button"
+													onclick={() => {
+														form.validate('slug');
+														editSlugOpen = false;
+													}}>{t`Save`}</Button
+												>
+											{/snippet}
+										</ResponsiveModal>
+									</InputGroup.Text>
+								</InputGroup.Addon>
+							{/if}
+						</InputGroup.Root>
 					{/snippet}
 				</Form.Control>
-			</Form.Field>
-			<Form.Field {form} name="slug">
-				<Form.Control>
-					{#snippet children({ props })}
-						<Form.Label>{t`Slug`}</Form.Label>
-						<Input {...props} bind:value={$data.slug} autocomplete="off" />
-						<Form.FieldErrors />
-					{/snippet}
-				</Form.Control>
+				<Form.FieldErrors />
 			</Form.Field>
 			<div class="flex justify-between gap-2">
 				<Button type="button" href="/organization" variant="outline" class="w-auto"
