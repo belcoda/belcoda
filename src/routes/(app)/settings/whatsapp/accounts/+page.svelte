2025555837760fb5a04c244712e2175924717717
Django Merope <dmerope@gmail.com>
2026-03-19 08:05:17 +0900

    Add embedded flow component to account settings page

--- src/routes/(app)/settings/whatsapp/accounts/+page.svelte
+++ src/routes/(app)/settings/whatsapp/accounts/+page.svelte
@@ -3,18 +3,9 @@
 	import ContentLayout from '$lib/components/layouts/app/ContentLayout.svelte';
 	import * as Empty from '$lib/components/ui/empty/index.js';
 	import DownloadIcon from '@lucide/svelte/icons/download';
+	import EmbeddedFlow from './EmbeddedFlow.svelte';
 </script>
 
 <ContentLayout rootLink="/settings/people/exports">
-	<Empty.Root>
-		<Empty.Header>
-			<Empty.Media variant="icon">
-				<DownloadIcon />
-			</Empty.Media>
-			<Empty.Title>{t`WhatsApp accounts`}</Empty.Title>
-			<Empty.Description
-				>{t`WhatsApp accounts are not currently supported, but will be added in a future update. `}</Empty.Description
-			>
-		</Empty.Header>
-	</Empty.Root>
+	<EmbeddedFlow />
 </ContentLayout>
