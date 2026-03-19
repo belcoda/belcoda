4e445dfe8571eb06e22d6ede21d241f4f57a0093
Django Merope <dmerope@gmail.com>
2026-03-19 22:39:40 +0900

    Components for editing templates

---
+++ src/lib/components/forms/whatsapp/templates/EditComponentText.svelte
@@ -0,0 +1,147 @@
+<script lang="ts">
+	import { t } from '$lib/index.svelte';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import XIcon from '@lucide/svelte/icons/x';
+
+	import { watch } from 'runed';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	let {
+		value = $bindable(),
+		items = $bindable([]),
+		type = 'body'
+	}: {
+		type?: 'header' | 'body';
+		value: string;
+		items: string[];
+	} = $props();
+
+	function syncVariablesWithValueString(value: string, variables: string[]): string[] {
+		// Extract all unique {{param_name}} patterns from the string
+		const matches = Array.from(new Set([...value.matchAll(/{{([1-9])}}/g)].map((m) => m[1])));
+
+		// Ensure matches and variables arrays are the same length
+		while (variables.length < matches.length) {
+			variables.push(''); // Add empty strings for any missing variables
+		}
+		// Trim variables array if longer than matches
+		if (variables.length > matches.length) {
+			variables.length = matches.length;
+		}
+
+		// If type is header, only keep the first variable
+		if (type === 'header' && variables.length > 1) {
+			variables = [variables[0]];
+		}
+
+		return variables;
+	}
+
+	watch(
+		() => value,
+		() => {
+			items = syncVariablesWithValueString(value, items);
+		}
+	);
+
+	function addNewVariable() {
+		if (type === 'header' && items.length >= 1) {
+			return; // Don't add more variables for header type
+		}
+		value = value + `{{${(items?.length ?? 0) + 1}}}`;
+		items.push('');
+	}
+
+	function deleteVariable(index: number) {
+		const variableToRemove = index + 1;
+		// Remove the variable from the value string
+		value = value.replace(`{{${variableToRemove}}}`, '');
+		// Remove the variable from items array
+		items.splice(index, 1);
+	}
+
+	function getValue() {
+		return value;
+	}
+	function setValue(incomingValue: string) {
+		value = incomingValue;
+		items = syncVariablesWithValueString(value, items);
+	}
+
+	import * as Alert from '$lib/components/ui/alert/index.js';
+</script>
+
+<div class="w-full">
+	<div>
+		<textarea
+			bind:value={getValue, setValue}
+			maxlength={type === 'header' ? 60 : 1024}
+			class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 invalid:outline-red-600 focus:border-gray-400 focus:ring-0 focus:outline-0 sm:text-sm/6"
+		></textarea>
+		<div class="ms-1 text-sm text-gray-500">
+			{type === 'header'
+				? t`The header text must be 60 characters or less`
+				: t`The body text must be 1024 characters or less`}
+		</div>
+	</div>
+	<div class="mt-2">
+		{@render variablesTable(items)}
+	</div>
+</div>
+
+{#snippet variablesTable(items: string[])}
+	<div class="relative w-full overflow-x-auto">
+		<div class="flex items-center justify-end gap-2">
+			{#if type === 'header' && items.length >= 1}
+				<span class="text-sm text-gray-500">{t`Template headers can only have 1 variable`}</span>
+			{/if}
+			<button
+				onclick={addNewVariable}
+				class="btn"
+				disabled={type === 'header' && items.length >= 1}
+			>
+				{t`Add variable`}
+			</button>
+		</div>
+		{#if items.length > 0}
+			<Alert.Root>
+				<Alert.Title>{t`Example values`}</Alert.Title>
+				<Alert.Description
+					>{t`Provide examples of the values you might use in your templates. These will be used in the WhatsApp template review process. You will be able to change these values when you send a message.`}</Alert.Description
+				>
+			</Alert.Root>
+		{:else}
+			<Alert.Root>
+				<Alert.Title>{t`Variables`}</Alert.Title>
+				<Alert.Description
+					>{t`Variables are placeholders for dynamic values that will be replaced with actual values when you send a message. You can add variables using the "Add new variable" button or by typing text in double curly brackets.`}</Alert.Description
+				>
+			</Alert.Root>
+		{/if}
+		<table class="w-full text-left text-sm text-gray-500 rtl:text-right">
+			<tbody>
+				{#each items as _, i}
+					<tr class="border-b border-gray-200">
+						<th scope="row" class="px-6 py-2 font-medium whitespace-nowrap text-gray-900">
+							{`{{${i + 1}}}`}
+						</th>
+						<td class="px-6 py-2">
+							<Input bind:value={items[i]} />
+						</td>
+						<td class="px-6 py-2">
+							<Button
+								onclick={() => deleteVariable(i)}
+								aria-label={t`Delete variable`}
+								size="icon-sm"
+								variant="ghost"
+								title={t`Delete variable`}
+							>
+								<XIcon class="size-4" />
+							</Button>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+{/snippet}
