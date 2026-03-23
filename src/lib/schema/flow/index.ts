f60a555aa8be387fc61133b736cffbd9e6042b48
Django Merope <dmerope@gmail.com>
2026-03-23 19:56:22 +0900

    Create flow schema

--- src/lib/schema/flow/index.ts
+++ src/lib/schema/flow/index.ts
@@ -1,14 +1,92 @@
 import { type XYPosition } from '@xyflow/svelte';
+import { type FilterGroupType, filterGroup } from '$lib/schema/person/filter';
 
-export type Flow = {
-	nodes: Node[];
-	edges: Edge[];
-};
+import * as v from 'valibot';
+import * as helpers from '$lib/schema/helpers';
 
-export type Node = {
-	id: string;
-	type: string;
-	position: XYPosition;
-};
+const nodeType = v.picklist(['message', 'eventSignup', 'tagAdd', 'targeting', 'templateMessage']);
 
-export type Edge = {};
+const nodeBase = v.object({
+	id: helpers.uuid,
+	position: v.object({
+		x: v.number(),
+		y: v.number()
+	})
+});
+
+const eventSignupNode = v.object({
+	...nodeBase.entries,
+	type: v.literal('eventSignup'),
+	data: v.object({
+		eventId: helpers.uuid
+	})
+});
+
+const tagAddNode = v.object({
+	...nodeBase.entries,
+	type: v.literal('tagAdd'),
+	data: v.object({
+		tagId: helpers.uuid
+	})
+});
+
+const targetingNode = v.object({
+	...nodeBase.entries,
+	type: v.literal('targeting'),
+	data: v.object({
+		filterGroup: filterGroup
+	})
+});
+
+const messageNode = v.object({
+	...nodeBase.entries,
+	type: v.literal('message'),
+	data: v.object({
+		text: helpers.mediumString,
+		imageUrl: v.optional(helpers.url),
+		buttons: v.array(v.object({ id: helpers.uuid, label: helpers.mediumString }))
+	})
+});
+
+const templateMessageNode = v.object({
+	...nodeBase.entries,
+	type: v.literal('templateMessage'),
+	data: v.object({
+		templateId: helpers.uuid,
+		header: v.optional(
+			v.object({
+				templateStrings: v.optional(v.array(helpers.shortString)),
+				imageUrl: v.optional(helpers.url)
+			})
+		),
+		body: v.optional(
+			v.object({
+				templateStrings: v.optional(v.array(helpers.shortString))
+			})
+		),
+		buttons: v.optional(v.array(v.object({ id: helpers.uuid })))
+	})
+});
+
+const nodeSchema = v.variant('type', [
+	eventSignupNode,
+	tagAddNode,
+	targetingNode,
+	messageNode,
+	templateMessageNode
+]);
+
+const edgeSchema = v.object({
+	type: v.literal('edge'),
+	source: helpers.uuid,
+	target: helpers.uuid,
+	sourceHandle: v.optional(helpers.uuid),
+	targetHandle: v.optional(helpers.uuid)
+});
+
+export const flowSchema = v.object({
+	nodes: v.array(nodeSchema),
+	edges: v.array(edgeSchema)
+});
+
+export type Flow = v.InferOutput<typeof flowSchema>;
