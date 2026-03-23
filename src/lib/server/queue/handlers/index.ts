d7bae6daebdff67983443f8680e4ef01e452627f
Django Merope <dmerope@gmail.com>
2026-03-23 19:57:25 +0900

    Add process flow node handler to queue

--- src/lib/server/queue/handlers/index.ts
+++ src/lib/server/queue/handlers/index.ts
@@ -4,3 +4,4 @@ export { importPeople } from '$lib/server/queue/handlers/people/import_people';
 export { deployEventWhatsAppFlow } from '$lib/server/queue/handlers/event/deployFlow';
 export { handleIncomingMessage } from '$lib/server/queue/handlers/whatsapp/incoming_message';
 export { handleWhatsappTemplateReviewed } from '$lib/server/queue/handlers/whatsapp/template_reviewed';
+export { processFlowNodeAction } from '$lib/server/queue/handlers/whatsapp/process_flow_node';
