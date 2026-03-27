4f9fc08be36c7194beb91356be2b2ba898049297
Django Merope <dmerope@gmail.com>
2026-03-27 14:48:12 +0900

    Proper preferences page (previously was test content)

--- src/routes/(app)/preferences/+page.svelte
+++ src/routes/(app)/preferences/+page.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-	import ContentLayout from '$lib/components/layouts/app/ContentLayout.svelte';
-	import { Button } from '$lib/components/ui/button/index.js';
-	import EllipsisIcon from '@lucide/svelte/icons/ellipsis';
 	import { t } from '$lib/index.svelte';
+	import ContentLayout from '$lib/components/layouts/app/ContentLayout.svelte';
+	import CogIcon from '@lucide/svelte/icons/cog';
+	import * as Empty from '$lib/components/ui/empty/index.js';
 </script>
 
-<ContentLayout rootLink="/preferences" {header} {footer}>sws</ContentLayout>
+<ContentLayout rootLink="/">
+	{#snippet header()}
+		<div class="flex items-center justify-between">
+			<h1 class="text-2xl font-bold">{t`Preferences`}</h1>
+		</div>
+	{/snippet}
 
-{#snippet header()}
-	<div class="flex items-center justify-between">
-		<h1>{t`Test`}</h1>
-		<Button variant="outline"><EllipsisIcon class="size-5" /></Button>
-	</div>
-{/snippet}
-
-{#snippet footer()}
-	<p>{t`Footer`}</p>
-{/snippet}
+	<Empty.Root>
+		<Empty.Header>
+			<Empty.Media variant="icon">
+				<CogIcon />
+			</Empty.Media>
+			<Empty.Title>{t`User preferences`}</Empty.Title>
+			<Empty.Description>{t`Manage your user preferences, including language`}</Empty.Description>
+		</Empty.Header>
+	</Empty.Root>
+</ContentLayout>
