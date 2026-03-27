63a68d6a82818cff382de68e6e8cdb1538ed0f3a
Django Merope <dmerope@gmail.com>
2026-03-27 14:46:11 +0900

    Hide communications if not admin
    We will want to allow nonadmins to send comms in the future, but don't
    currently have a simple way to implement it and it can be some sensitive
    permissions, so need to talk to users to get consensus

--- src/lib/components/layouts/app/navigation/MobileBottomNav.svelte
+++ src/lib/components/layouts/app/navigation/MobileBottomNav.svelte
@@ -6,32 +6,38 @@
 		buttonVariants({ variant: 'ghost', size: 'default' }),
 		'h-full w-full justify-center rounded-none'
 	);
+	import { appState } from '$lib/state.svelte';
 </script>
 
 <div class="bg-neutral-primary-soft border-default h-full w-full border-t">
-	<div class="mx-auto grid h-full max-w-lg grid-cols-4 font-medium">
+	<div
+		class="mx-auto grid h-full max-w-lg grid-cols-3 font-medium"
+		class:grid-cols-4={appState.isAdminOrOwner}
+	>
 		<a href="/community" class={navClass}>
 			<div class="text-center">
 				<div class="icon-[lucide--users] size-6">&nbsp;</div>
-				<div class="text-body group-hover:text-fg-brand mt-1 text-xs">Community</div>
+				<div class="text-body group-hover:text-fg-brand mt-1 text-xs">{t`Community`}</div>
 			</div>
 		</a>
-		<a href="/communications" class={navClass}>
-			<div class="text-center">
-				<div class="icon-[flowbite--messages-outline] size-6">&nbsp;</div>
-				<div class="text-body group-hover:text-fg-brand mt-1 text-xs">Communications</div>
-			</div>
-		</a>
+		{#if appState.isAdminOrOwner}
+			<a href="/communications" class={navClass}>
+				<div class="text-center">
+					<div class="icon-[flowbite--messages-outline] size-6">&nbsp;</div>
+					<div class="text-body group-hover:text-fg-brand mt-1 text-xs">{t`Communications`}</div>
+				</div>
+			</a>
+		{/if}
 		<a href="/events" class={navClass}>
 			<div class="text-center">
 				<div class="icon-[mdi--events] size-6">&nbsp;</div>
-				<div class="text-body group-hover:text-fg-brand mt-1 text-xs">Events</div>
+				<div class="text-body group-hover:text-fg-brand mt-1 text-xs">{t`Events`}</div>
 			</div>
 		</a>
 		<a href="/petitions" class={navClass}>
 			<div class="text-center">
 				<div class="icon-[mage--zap] size-6">&nbsp;</div>
-				<div class="text-body group-hover:text-fg-brand mt-1 text-xs">Petitions</div>
+				<div class="text-body group-hover:text-fg-brand mt-1 text-xs">{t`Petitions`}</div>
 			</div>
 		</a>
 	</div>
