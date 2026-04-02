927769490456c54c49abcecf336215376ffbb285
Django Merope <dmerope@gmail.com>
2026-04-02 17:22:56 +0900

    Use the new vite export in base wuchale package

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { wuchale } from '@wuchale/vite-plugin';
+import { wuchale } from 'wuchale/vite';
 import { playwright } from '@vitest/browser-playwright';
 
 import dotenv from 'dotenv';
