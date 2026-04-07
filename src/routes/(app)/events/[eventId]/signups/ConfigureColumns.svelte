e955011d46da8794dca2df1b0cb94b39de0a5e58
Django Merope <dmerope@gmail.com>
2026-04-07 17:10:34 +0900

    Add conditional handling of survey qyuestinos for signups table

--- src/routes/(app)/events/[eventId]/signups/ConfigureColumns.svelte
+++ src/routes/(app)/events/[eventId]/signups/ConfigureColumns.svelte
@@ -13,7 +13,8 @@
 	}: { person: Columns['person']; custom: Columns['custom']; event: ReadEventZero } = $props();
 	import { getSurveyQuestions } from '$lib/components/forms/event/survey_actions';
 	/* svelte-ignore state_referenced_locally */
-	const customQuestions = getSurveyQuestions(event.settings.survey.collections[0].questions).custom;
+	const surveyQuestions = event.settings.survey?.collections?.[0]?.questions ?? [];
+	const customQuestions = getSurveyQuestions(surveyQuestions).custom;
 	import { Checkbox } from '$lib/components/ui/checkbox/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 	import { renderColumnName } from './actions';
