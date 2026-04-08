2e01c5ad1c91823bd7f79a176a90e849e98572a8
Django Merope <dmerope@gmail.com>
2026-04-08 23:42:54 +0900

    Throw redirect rahter than returning it

--- src/routes/(app)/communications/email/drafts/new/+page.ts
+++ src/routes/(app)/communications/email/drafts/new/+page.ts
@@ -1,9 +1,10 @@
 import { v7 as uuidv7 } from 'uuid';
 import { redirect } from '@sveltejs/kit';
+
 export async function load() {
 	const emailId = uuidv7();
-	redirect(302, `/communications/email/drafts/${emailId}?defaultCreateMode=true`);
 	// defaultCreateMode is used to indicate that this is a new email draft.
 	// This is just a hint to allow the page to bootstrap the new email composer before checking zero to see if the email exists.
 	// The check will still happen, and if there is an email with the same id, it will be used to populate the composer.
+	throw redirect(302, `/communications/email/drafts/${emailId}?defaultCreateMode=true`);
 }
