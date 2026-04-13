f29e6361c6588360b4100f72c2ac06d95321ce82
Django Merope <dmerope@gmail.com>
2026-04-13 22:55:58 +0900

    Client and server mutators for emoji reactions

---
+++ src/lib/server/api/mutate/whatsapp_message.ts
@@ -0,0 +1,17 @@
+import { emojiReactionMutatorSchema } from '$lib/schema/whatsapp-message';
+import { defineMutator } from '@rocicorp/zero';
+import * as dataFunctions from '$lib/server/api/data/whatsapp/message';
+
+export const emojiReaction = defineMutator(
+	emojiReactionMutatorSchema,
+	async ({ tx, args, ctx }) => {
+		if (tx.location !== 'server') {
+			throw new Error('emojiReaction can only be called from the server');
+		}
+		await dataFunctions.emojiReaction({
+			tx,
+			ctx,
+			args: args
+		});
+	}
+);
