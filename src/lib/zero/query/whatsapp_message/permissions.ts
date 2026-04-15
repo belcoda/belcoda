0a52b304c6eabc0e708f88f7ad57369084a067ee
Django Merope <dmerope@gmail.com>
2026-04-15 16:56:54 +0900

    Update comment to reflect permissions logic
    The permissions logic allows any member of an organization to view a
    whastapp message. This PR updates the comment to reflect that.
    
    It is probably more permissive than we want, and these permissions may
    change in the future.

--- src/lib/zero/query/whatsapp_message/permissions.ts
+++ src/lib/zero/query/whatsapp_message/permissions.ts
@@ -2,7 +2,7 @@ import { type ExpressionBuilder } from '@rocicorp/zero';
 import type { Schema, QueryContext } from '$lib/zero/schema';
 
 // it should return a boolean expression that is used to filter the whatsapp_message table
-// it should return true if the user is an admin or owner of the organization of the whatsapp message
+// it should return true if the user is an admin or owner or any kind of member of the organization of the whatsapp message
 // it should return false otherwise
 
 export function whatsappMessageReadPermissions(
