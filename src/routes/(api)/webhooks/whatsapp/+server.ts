c68aea8388aea2b97003f97ff1e401bb1f5cc409
Kenneth Lebu <kenlebu@gmail.com>
2026-04-16 16:20:29 +0300

    skip token verification if in mock mode

--- src/routes/(api)/webhooks/whatsapp/+server.ts
+++ src/routes/(api)/webhooks/whatsapp/+server.ts
@@ -5,29 +5,48 @@ import { drizzle } from '$lib/server/db';
 import { whatsappLog } from '$lib/schema/drizzle';
 import { env } from '$env/dynamic/private';
 import { v7 as uuidv7 } from 'uuid';
+import { handleIncomingMessage } from '$lib/server/queue/handlers/whatsapp/incoming_message';
+import { handleWhatsappTemplateReviewed } from '$lib/server/queue/handlers/whatsapp/template_reviewed';
 const webhookSecret = env.YCLOUD_WEBHOOK_VERIFY_TOKEN;
+const isMockMode = env.MOCK_EXTERNAL_SERVICES === 'true';
 
 export async function POST({ request, url }) {
 	const verifyToken = url.searchParams.get('verify');
-	if (verifyToken !== webhookSecret) {
+	if (!isMockMode && verifyToken !== webhookSecret) {
 		return new Response('Unauthorized', { status: 401 });
 	}
 	const body = await request.json();
 	try {
-		const queue = await getQueue();
-		switch (body.type) {
-			case 'whatsapp.template.reviewed':
-				await queue.handleWhatsappTemplateReviewed(body);
-				break;
-			case 'whatsapp.inbound_message.received':
-				await queue.handleIncomingMessage(body);
-				break;
-			case 'whatsapp.message.updated':
-				//TODO: implement message updated logic when needed
-				//await queue.handleWhatsappMessageUpdated(body);
-				break;
-			default:
-				log.warn({ type: body.type }, 'Unknown whatsapp webhook type');
+		if (isMockMode) {
+			switch (body.type) {
+				case 'whatsapp.template.reviewed':
+					await handleWhatsappTemplateReviewed(body);
+					break;
+				case 'whatsapp.inbound_message.received':
+					await handleIncomingMessage(body);
+					break;
+				case 'whatsapp.message.updated':
+					//TODO: implement message updated logic when needed
+					break;
+				default:
+					log.warn({ type: body.type }, 'Unknown whatsapp webhook type');
+			}
+		} else {
+			const queue = await getQueue();
+			switch (body.type) {
+				case 'whatsapp.template.reviewed':
+					await queue.handleWhatsappTemplateReviewed(body);
+					break;
+				case 'whatsapp.inbound_message.received':
+					await queue.handleIncomingMessage(body);
+					break;
+				case 'whatsapp.message.updated':
+					//TODO: implement message updated logic when needed
+					//await queue.handleWhatsappMessageUpdated(body);
+					break;
+				default:
+					log.warn({ type: body.type }, 'Unknown whatsapp webhook type');
+			}
 		}
 	} catch (error) {
 		log.error(error, 'Error processing whatsapp webhook');
