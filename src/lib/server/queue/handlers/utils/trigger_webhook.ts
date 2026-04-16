48eb9c2dec79f4658ca0583d5dfe14de32e24cd7
Django Merope <dmerope@gmail.com>
2026-04-16 22:52:17 +0900

    Make a bunch of changes to improve error handling and robustness of handler

--- src/lib/server/queue/handlers/utils/trigger_webhook.ts
+++ src/lib/server/queue/handlers/utils/trigger_webhook.ts
@@ -2,7 +2,7 @@ import { parse } from 'valibot';
 import { CURRENT_API_VERSION } from '$lib/schema/helpers';
 import { webhookPayloadSchema, type WebhookPayload } from '$lib/schema/webhook';
 
-import { db } from '$lib/server/db';
+import { db, drizzle } from '$lib/server/db';
 
 import pino from '$lib/pino';
 const log = pino(import.meta.url);
@@ -22,77 +22,90 @@ export async function triggerWebhook({
 		const parsed = parse(webhookPayloadSchema, payload);
 		const eventType = parsed.type;
 
-		await db.transaction(async (tx) => {
-			const returnedWebhooks = await tx.dbTransaction.wrappedTransaction.query.webhook.findMany({
-				where: (row, { and, eq }) =>
-					and(
-						eq(row.organizationId, organizationId),
-						sql`${row.eventTypes} ? ${eventType} OR ${row.eventTypes} ? 'all'`,
-						eq(row.enabled, true)
-					)
-			});
-			for (const webhook of returnedWebhooks) {
-				try {
-					const webhookBody = {
-						id: uuidv7(),
+		const returnedWebhooks = await drizzle.query.webhook.findMany({
+			where: (row, { and, eq }) =>
+				and(
+					eq(row.organizationId, organizationId),
+					sql`${row.eventTypes} ? ${eventType} OR ${row.eventTypes} ? 'all'`,
+					eq(row.enabled, true)
+				)
+		});
+		for (const webhook of returnedWebhooks) {
+			const webhookBody = {
+				id: uuidv7(),
+				type: eventType,
+				createdAt: new Date().toISOString(),
+				apiVersion: CURRENT_API_VERSION,
+				data: parsed.data
+			};
+			let responseText: string = 'NOT_SENT';
+			let httpStatusCode: number = 418;
+			let responseOk: boolean = false;
+			try {
+				const controller = new AbortController();
+				const timeoutId = setTimeout(() => controller.abort(), 15000); // 15s timeout
+				const response = await fetch(webhook.targetUrl, {
+					method: 'POST',
+					signal: controller.signal,
+					headers: {
+						'Content-Type': 'application/json',
+						'X-Webhook-Secret': webhook.secret
+					},
+					body: JSON.stringify(webhookBody)
+				});
+				clearTimeout(timeoutId);
+				responseText = await response.text();
+				httpStatusCode = response.status;
+				responseOk = response.ok;
+				if (responseOk) {
+					await drizzle
+						.update(webhookTable)
+						.set({
+							lastSuccessAt: new Date()
+						})
+						.where(eq(webhookTable.id, webhook.id));
+				} else {
+					throw new Error(
+						`Webhook failed [${response.status} ${response.statusText}]: ${responseText}`
+					);
+				}
+			} catch (error) {
+				log.error(
+					{
+						status: `${httpStatusCode} ${responseText}`,
+						webhook,
+						error,
 						type: eventType,
-						createdAt: new Date().toISOString(),
-						apiVersion: CURRENT_API_VERSION,
-						data: parsed.data
-					};
-
-					const response = await fetch(webhook.targetUrl, {
-						method: 'POST',
-						headers: {
-							'Content-Type': 'application/json',
-							'X-Webhook-Secret': webhook.secret
-						},
-						body: JSON.stringify(webhookBody)
-					});
-					if (response.ok) {
-						await tx.dbTransaction.wrappedTransaction
-							.update(webhookTable)
-							.set({
-								lastSuccessAt: new Date()
-							})
-							.where(eq(webhookTable.id, webhook.id));
-					} else {
-						log.error(
-							{
-								status: `${response.status} ${response.statusText}`,
-								webhook,
-								type: eventType,
-								payload: parsed.data,
-								organizationId
-							},
-							'Failed to trigger webhook'
-						);
-						await tx.dbTransaction.wrappedTransaction
-							.update(webhookTable)
-							.set({
-								lastFailureAt: new Date()
-							})
-							.where(eq(webhookTable.id, webhook.id));
-					}
-
-					const responseText = await response.text();
+						payload: parsed.data,
+						organizationId
+					},
+					'Failed to trigger webhook'
+				);
+				await drizzle
+					.update(webhookTable)
+					.set({
+						lastFailureAt: new Date()
+					})
+					.where(eq(webhookTable.id, webhook.id));
+			} finally {
+				try {
 					const logToInsert: typeof webhookLog.$inferInsert = {
 						id: uuidv7(),
 						webhookId: webhook.id,
 						eventType: eventType,
-						status: response.ok ? 'success' : 'failed',
+						status: responseOk ? 'success' : 'failed',
 						payload: webhookBody,
-						httpStatusCode: response.status,
+						httpStatusCode: httpStatusCode,
 						responseBody: responseText,
 						attemptNumber: 1,
 						createdAt: new Date()
 					};
-					await tx.dbTransaction.wrappedTransaction.insert(webhookLog).values(logToInsert);
+					await drizzle.insert(webhookLog).values(logToInsert);
 				} catch (error) {
-					log.error({ error, webhook, payload, organizationId }, 'Failed to trigger webhook');
+					log.error({ error, webhook, payload, organizationId }, 'Failed to insert webhook log');
 				}
 			}
-		});
+		}
 	} catch (error) {
 		log.error({ error, payload, organizationId }, 'Failed to trigger webhook');
 	}
