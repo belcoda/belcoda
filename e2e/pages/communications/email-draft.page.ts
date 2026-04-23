9d68cf42d89005b3b0a5b3c2363c33e1c7fa63e4
Kenneth Lebu <kenlebu@gmail.com>
2026-04-23 13:54:45 +0300

    create page objects

---
+++ e2e/pages/communications/email-draft.page.ts
@@ -0,0 +1,70 @@
+import type { Locator, Page } from '@playwright/test';
+
+export class EmailDraftPage {
+	readonly page: Page;
+	readonly draftPage: Locator;
+	readonly form: Locator;
+	readonly subjectInput: Locator;
+	readonly discardButton: Locator;
+	readonly saveButton: Locator;
+	readonly testEmailToggleButton: Locator;
+	readonly sendButton: Locator;
+	readonly testEmailForm: Locator;
+	readonly testEmailAddressInput: Locator;
+	readonly testEmailSendButton: Locator;
+
+	constructor(page: Page) {
+		this.page = page;
+		this.draftPage = page.getByTestId('email-draft-page');
+		this.form = page.getByTestId('email-form');
+		this.subjectInput = page.getByTestId('email-form-subject');
+		this.discardButton = page.getByTestId('email-draft-discard');
+		this.saveButton = page.getByTestId('email-draft-save');
+		this.testEmailToggleButton = page.getByTestId('email-draft-test-toggle');
+		this.sendButton = page.getByTestId('email-draft-send');
+		this.testEmailForm = page.getByTestId('email-test-form');
+		this.testEmailAddressInput = page.getByTestId('email-test-address');
+		this.testEmailSendButton = page.getByTestId('email-test-send');
+	}
+
+	async gotoNewDraft() {
+		await this.page.goto('/communications/email/drafts/new');
+	}
+
+	async gotoDraftById(id: string) {
+		await this.page.goto(`/communications/email/drafts/${id}`);
+	}
+
+	async waitForLoaded() {
+		await this.draftPage.waitFor({ state: 'visible', timeout: 15_000 });
+		await this.form.waitFor({ state: 'visible', timeout: 15_000 });
+	}
+
+	async fillSubject(subject: string) {
+		await this.subjectInput.fill(subject);
+	}
+
+	async save() {
+		await this.saveButton.click();
+	}
+
+	async send() {
+		await this.sendButton.click();
+	}
+
+	async discardAndConfirm() {
+		this.page.once('dialog', (dialog) => dialog.accept());
+		await this.discardButton.click();
+	}
+
+	async openTestEmailForm() {
+		await this.testEmailToggleButton.click();
+		await this.testEmailForm.waitFor({ state: 'visible', timeout: 10_000 });
+	}
+
+	async sendTestEmail(emailAddress: string) {
+		await this.openTestEmailForm();
+		await this.testEmailAddressInput.fill(emailAddress);
+		await this.testEmailSendButton.click();
+	}
+}
