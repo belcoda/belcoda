98fcc349b39d138f0741b3f4673d3d5501d06e46
Django Merope <dmerope@gmail.com>
2026-04-27 17:19:45 +0900

    Only throw errors when not building

--- src/routes/(api)/api/docs/webhooks/+server.ts
+++ src/routes/(api)/api/docs/webhooks/+server.ts
@@ -3,7 +3,8 @@ import { ScalarApiReference } from '@scalar/sveltekit';
 import type { RequestHandler } from './$types';
 import { env } from '$env/dynamic/public';
 const host = env.PUBLIC_HOST?.replace(/\/$/, '');
-if (!host) {
+import { building } from '$app/environment';
+if (!host && !building) {
 	throw new Error('webhooks docs: PUBLIC_HOST is not configured');
 }
 const render = ScalarApiReference({
