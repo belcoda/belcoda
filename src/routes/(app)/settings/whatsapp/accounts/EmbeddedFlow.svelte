76c879dda564844e2a8b65a17dfec22029a7d5cc
Django Merope <dmerope@gmail.com>
2026-05-08 16:09:26 +0900

    Add eslint disable comment

--- src/routes/(app)/settings/whatsapp/accounts/EmbeddedFlow.svelte
+++ src/routes/(app)/settings/whatsapp/accounts/EmbeddedFlow.svelte
@@ -66,6 +66,7 @@
 			if (data.type !== 'WA_EMBEDDED_SIGNUP') return;
 
 			if (data.event === 'FINISH') {
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
 				const { phone_number_id, waba_id, business_id } = data.data;
 				if (appState.organizationId && appState.activeOrganization.data) {
 					const result = z.mutate(
