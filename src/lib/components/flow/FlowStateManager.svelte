2c67da0ec2afed6b313aad7be2b77e10e71095a3
Django Merope <dmerope@gmail.com>
2026-05-21 22:24:34 +0900

    Helper component to manage state lifecycle of parent flow

---
+++ src/lib/components/flow/FlowStateManager.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	//*
+	// This component is responsible for making sure that the flow state is updated with the context in which the parent component is called changes, but the component doesn't get remounted.
+	// For example, if the Flow is being used in a page, and the user navigates to the same page with different params, the FlowStateManager should update the flow state with the new context, even though the component itself doesn't get remounted.
+	// */
+	import { setNodes, setEdges } from '$lib/components/flow/flow_state.svelte';
+	import { onMount, onDestroy } from 'svelte';
+	import type { Node, Edge } from '@xyflow/svelte';
+	const {
+		loadFlowFunction
+	}: { loadFlowFunction: () => Promise<{ nodes: Node[]; edges: Edge[] }> } = $props();
+	onMount(async () => {
+		const { nodes, edges } = await loadFlowFunction();
+		setNodes(nodes);
+		setEdges(edges);
+	});
+	onDestroy(() => {
+		setNodes([]);
+		setEdges([]);
+	});
+</script>
