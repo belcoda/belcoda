10acb25e4cd706b0f941fab372da409d9e768225
Django Merope <dmerope@gmail.com>
2026-05-21 22:56:10 +0900

    Create a separate component for sending a individual message

---
+++ src/lib/components/widgets/communications/send_wa_msg/SendBusinessApiIndividualMessage.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import * as InputGroup from '$lib/components/ui/input-group/index.js';
+	const { personId }: { personId: string } = $props();
+	import PlusIcon from '@lucide/svelte/icons/plus';
+	import ArrowUpIcon from '@lucide/svelte/icons/arrow-up';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import ImagePlusIcon from '@lucide/svelte/icons/image-plus';
+</script>
+
+<InputGroup.Root>
+	<InputGroup.Textarea placeholder="Write your message..." />
+	<InputGroup.Addon align="block-end">
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger>
+				<InputGroup.Button variant="outline" class="rounded-full" size="icon-xs">
+					<PlusIcon />
+				</InputGroup.Button>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				<DropdownMenu.Item><ImagePlusIcon /> Add image</DropdownMenu.Item>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+		<InputGroup.Button variant="default" class="ml-auto rounded-full" size="icon-xs" disabled>
+			<ArrowUpIcon />
+			<span class="sr-only">Send</span>
+		</InputGroup.Button>
+	</InputGroup.Addon>
+</InputGroup.Root>
