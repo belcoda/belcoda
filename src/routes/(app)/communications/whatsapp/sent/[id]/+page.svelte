a3430838375070160620ab550f7e9c86f2a8d98b
Django Merope <dmerope@gmail.com>
2026-05-21 22:35:20 +0900

    Update sent to use new state model

--- src/routes/(app)/communications/whatsapp/sent/[id]/+page.svelte
+++ src/routes/(app)/communications/whatsapp/sent/[id]/+page.svelte
@@ -3,39 +3,37 @@
 	import { z } from '$lib/zero.svelte';
 	import queries from '$lib/zero/query/index';
 	const { params } = $props();
-	import { t } from '$lib/index.svelte';
-	import { goto } from '$app/navigation';
-	import { mutators } from '$lib/zero/mutate/client_mutators';
-
-	const whatsappThreadQuery = $derived.by(() =>
-		z.createQuery(
-			queries.whatsappThread.read({
-				threadId: params.id
-			})
-		)
-	);
-	import { appState } from '$lib/state.svelte';
+	import { setNodes, setEdges } from '$lib/components/flow/flow_state.svelte';
+	import { onMount } from 'svelte';
+	let loading = $state(true);
+	async function loadFlow() {
+		try {
+			const whatsappThread = await z.run(
+				queries.whatsappThread.read({
+					threadId: params.id
+				})
+			);
+			if (whatsappThread) {
+				return { nodes: whatsappThread.flow.nodes, edges: whatsappThread.flow.edges };
+			} else {
+				return { nodes: [], edges: [] };
+			}
+		} catch (error) {
+			throw error;
+		} finally {
+			loading = false;
+		}
+	}
+	onMount(loadFlow);
 	import { Skeleton } from '$lib/components/ui/skeleton';
-	import { onDestroy } from 'svelte';
-	onDestroy(() => {
-		//if (whatsappThreadQuery?.details.type === 'complete' && whatsappThreadQuery?.data) {
-		//if whatsapp thread is EXACTLY deepEqual to the starting state, delete it.
-		//}
-		console.log('TODO: Destroy thread');
-	});
 </script>
 
 {#key params.id}
-	{#if whatsappThreadQuery?.details.type === 'complete' && whatsappThreadQuery?.data}
-		<Flow
-			backButtonUrl="/communications/whatsapp"
-			nodes={whatsappThreadQuery.data.flow.nodes}
-			edges={whatsappThreadQuery.data.flow.edges}
-			disabled={true}
-		/>
+	{#if loading}
+		<Skeleton class="h-48 w-full" />
+		<Skeleton class="h-48 w-full" />
+		<Skeleton class="h-48 w-full" />
 	{:else}
-		<Skeleton class="h-48 w-full" />
-		<Skeleton class="h-48 w-full" />
-		<Skeleton class="h-48 w-full" />
+		<Flow backButtonUrl="/communications/whatsapp" disabled={true} loadFlowFunction={loadFlow} />
 	{/if}
 {/key}
