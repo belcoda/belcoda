41b88ec06816aba6277dec47f53c82073835e507
Django Merope <dmerope@gmail.com>
2026-05-21 22:25:20 +0900

    Display saved state and avoid navigating away from unsaved state

---
+++ src/lib/components/flow/DisplaySavedState.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { Spinner } from '$lib/components/ui/spinner/index.js';
+	import CheckIcon from '@lucide/svelte/icons/check';
+	import { beforeNavigate } from '$app/navigation';
+	import { t } from '$lib/index.svelte';
+	import { Panel } from '@xyflow/svelte';
+	import { isLoading, isTainted, lastSavedAt } from '$lib/components/flow/flow_state.svelte';
+	import { getTimeAgo } from '$lib/utils/time';
+	import { locale } from '$lib/index.svelte';
+	const timeAgo = getTimeAgo(locale.current);
+	beforeNavigate((nav) => {
+		if (!isTainted()) return;
+		if (!window.confirm(t`Your changes might not be saved. Leave this page?`)) {
+			nav.cancel();
+		}
+	});
+
+	const derivedTimeAgo = $derived(t`Saved ${timeAgo.format(lastSavedAt())}`);
+</script>
+
+<Panel position="bottom-left">
+	<div class="text-xs text-muted-foreground">
+		{#if isLoading()}
+			<div class="flex items-center gap-2">
+				<Spinner />
+				{t`Saving...`}
+			</div>
+		{:else}
+			<div class="flex items-center gap-2">
+				<CheckIcon class="size-3" strokeWidth={3} />
+				{derivedTimeAgo}
+			</div>
+		{/if}
+	</div>
+</Panel>
