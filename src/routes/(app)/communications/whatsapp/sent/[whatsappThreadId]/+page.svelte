250159ccbb0857814696f37866a06ede29de559d
Django Merope <dmerope@gmail.com>
2026-05-25 16:38:17 +0900

    Move and simplify sent message loader

--- src/routes/(app)/communications/whatsapp/sent/[whatsappThreadId]/+page.svelte
+++ src/routes/(app)/communications/whatsapp/sent/[whatsappThreadId]/+page.svelte
@@ -3,14 +3,11 @@
 	import { z } from '$lib/zero.svelte';
 	import queries from '$lib/zero/query/index';
 	const { params } = $props();
-	import { setNodes, setEdges } from '$lib/components/flow/flow_state.svelte';
-	import { onMount } from 'svelte';
-	let loading = $state(true);
 	async function loadFlow() {
 		try {
 			const whatsappThread = await z.run(
 				queries.whatsappThread.read({
-					threadId: params.id
+					threadId: params.whatsappThreadId
 				})
 			);
 			if (whatsappThread) {
@@ -20,20 +17,11 @@
 			}
 		} catch (error) {
 			throw error;
-		} finally {
-			loading = false;
 		}
 	}
-	onMount(loadFlow);
 	import { Skeleton } from '$lib/components/ui/skeleton';
 </script>
 
-{#key params.id}
-	{#if loading}
-		<Skeleton class="h-48 w-full" />
-		<Skeleton class="h-48 w-full" />
-		<Skeleton class="h-48 w-full" />
-	{:else}
-		<Flow backButtonUrl="/communications/whatsapp" disabled={true} loadFlowFunction={loadFlow} />
-	{/if}
+{#key params.whatsappThreadId}
+	<Flow backButtonUrl="/communications/whatsapp" disabled={true} loadFlowFunction={loadFlow} />
 {/key}
