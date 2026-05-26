a04b6826fa0f759bf75ea7b5bffeb365874daa13
Kenneth Lebu <kenlebu@gmail.com>
2026-05-26 17:25:33 +0300

    read pagination mode from public env

--- src/lib/utils/pagination.ts
+++ src/lib/utils/pagination.ts
@@ -1,4 +1,6 @@
+import { PUBLIC_COMMUNITY_PAGINATION_MODE } from '$env/static/public';
+
 export type PaginationMode = 'button' | 'infinite';
 
 export const COMMUNITY_PAGINATION_MODE: PaginationMode =
-	import.meta.env.PUBLIC_COMMUNITY_PAGINATION_MODE === 'infinite' ? 'infinite' : 'button';
+	PUBLIC_COMMUNITY_PAGINATION_MODE === 'infinite' ? 'infinite' : 'button';
