f5b11bb154045ec7c2bd935856c1490c03504468
Django Merope <dmerope@gmail.com>
2026-05-27 13:10:07 +0900

    Update stripe customer id to make it unique

--- drizzle/0013_closed_the_captain.sql
+++ drizzle/0013_closed_the_captain.sql
@@ -1,2 +1,3 @@
 ALTER TABLE "organization" ADD COLUMN "stripe_customer_id" text;--> statement-breakpoint
-ALTER TABLE "organization" ADD COLUMN "billing_email" text;
+ALTER TABLE "organization" ADD COLUMN "billing_email" text;--> statement-breakpoint
+CREATE UNIQUE INDEX "organization_stripe_customer_id_unique" ON "organization"("stripe_customer_id") WHERE "stripe_customer_id" IS NOT NULL;
