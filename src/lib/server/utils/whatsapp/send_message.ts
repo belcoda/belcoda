8d27474a3f65a7749c3f42dc87cff097cb89c537
Django Merope <dmerope@gmail.com>
2026-06-09 16:07:18 +0900

    Remove duplicate billing block

--- src/lib/server/utils/whatsapp/send_message.ts
+++ src/lib/server/utils/whatsapp/send_message.ts
@@ -276,30 +276,6 @@ export async function sendWhatsappTemplateMessage({
 	if (!ycloudResponse.id) {
 		throw new Error('Failed to send message to YCloud');
 	}
-	await db.transaction(async (tx) => {
-		const claimedFreeCredit = await _reduceFreeWhatsAppMessageCredits({
-			organizationId: organization.id,
-			tx
-		});
-		if (!claimedFreeCredit) {
-			const delta = (ycloudResponse.totalPrice ?? 0) * 100; //convert to cents
-			const deltaInHundredthsOfCents = Math.ceil(delta * 100); //convert to hundredths of cents
-			await _createLedgerEntry({
-				tx,
-				args: {
-					organizationId: organization.id,
-					deltaInUsdHundredthsOfCents: -deltaInHundredthsOfCents,
-					metadata: {
-						type: 'whatsapp_message_outgoing',
-						whatsappMessageId: whatsappMessageId,
-						whatsappThreadId: threadId ?? null,
-						sentByUserId: sendingUserId ?? null,
-						teamId: null //for now, always null -- we don't currently support team messaging
-					}
-				}
-			});
-		}
-	});
 
 	//persist message in db
 	await db.transaction(async (tx) => {
