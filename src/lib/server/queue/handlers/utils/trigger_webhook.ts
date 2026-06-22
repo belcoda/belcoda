dabc4eff85ab3f0aa5b8400a4cf9c245dd305936
Django Merope <dmerope@gmail.com>
2026-06-22 23:13:28 +0900

    Split webhook queue handler into trigger and send (and only retry send)

--- src/lib/server/queue/handlers/utils/trigger_webhook.ts
+++ src/lib/server/queue/handlers/utils/trigger_webhook.ts
@@ -2,7 +2,7 @@ import { parse } from 'valibot';
 import { CURRENT_API_VERSION } from '$lib/schema/helpers';
 import { webhookPayloadSchema, type WebhookPayload } from '$lib/schema/webhook';
 
-import { db, drizzle } from '$lib/server/db';
+import { drizzle } from '$lib/server/db';
 
 import pino from '$lib/pino';
 const log = pino(import.meta.url);
@@ -26,25 +26,149 @@ function getRetryDelay(retryDelay: number, retryCount: number): number {
 	);
 }
 
-export async function triggerWebhook({
+export async function sendWebhook({
+	webhook,
+	eventType,
 	payload,
 	organizationId,
 	retryDelay = 0,
 	retryCount = 0
 }: {
+	webhook: typeof webhookTable.$inferSelect;
+	eventType: WebhookPayload['type'];
 	payload: WebhookPayload;
 	organizationId: string;
 	retryDelay?: number;
 	retryCount?: number;
+}) {
+	const parsed = parse(webhookPayloadSchema, payload);
+	const webhookBody = {
+		id: uuidv7(),
+		type: eventType,
+		createdAt: new Date().toISOString(),
+		apiVersion: CURRENT_API_VERSION,
+		data: parsed.data
+	};
+	let responseText: string = 'NOT_SENT';
+	let httpStatusCode: number = 418;
+	let responseOk: boolean = false;
+	const controller = new AbortController();
+	const timeoutId = setTimeout(() => controller.abort(), 15000); // 15s timeout
+	try {
+		const response = await fetch(webhook.targetUrl, {
+			method: 'POST',
+			signal: controller.signal,
+			headers: {
+				'Content-Type': 'application/json',
+				'X-Webhook-Secret': webhook.secret
+			},
+			body: JSON.stringify(webhookBody)
+		});
+
+		responseText = await response.text();
+		httpStatusCode = response.status;
+		responseOk = response.ok;
+		if (responseOk) {
+			await drizzle
+				.update(webhookTable)
+				.set({
+					lastSuccessAt: new Date()
+				})
+				.where(eq(webhookTable.id, webhook.id));
+		} else {
+			throw new Error(
+				`Webhook failed [${response.status} ${response.statusText}]: ${responseText}`
+			);
+		}
+	} catch (error) {
+		log.error(
+			{
+				status: `${httpStatusCode} ${responseText}`,
+				webhookId: webhook.id,
+				error,
+				type: eventType,
+				payload: parsed.data,
+				organizationId,
+				retryDelay,
+				retryCount
+			},
+			'Failed to trigger webhook'
+		);
+		await drizzle
+			.update(webhookTable)
+			.set({
+				lastFailureAt: new Date()
+			})
+			.where(eq(webhookTable.id, webhook.id));
+
+		if (retryCount < MAX_RETRY_COUNT) {
+			const queue = await getQueue();
+			const newRetryDelay = getRetryDelay(retryDelay, retryCount);
+			log.info(
+				{
+					webhookId: webhook.id,
+					eventType,
+					payload,
+					organizationId,
+					retryDelay,
+					retryCount,
+					newRetryDelay
+				},
+				'Retrying webhook'
+			);
+			await queue.sendWebhook(
+				{
+					webhook,
+					eventType,
+					organizationId,
+					payload,
+					retryDelay: newRetryDelay,
+					retryCount: retryCount + 1
+				},
+				{
+					startAfter: newRetryDelay //in seconds
+				}
+			);
+		} else {
+			log.error(
+				{ webhookId: webhook.id, eventType, payload, organizationId, retryDelay, retryCount },
+				'CRITICAL: Webhook failed after max retries'
+			);
+		}
+	} finally {
+		clearTimeout(timeoutId);
+		try {
+			const logToInsert: typeof webhookLog.$inferInsert = {
+				id: uuidv7(),
+				webhookId: webhook.id,
+				eventType: eventType,
+				status: responseOk ? 'success' : 'failed',
+				payload: webhookBody,
+				httpStatusCode: httpStatusCode,
+				responseBody: responseText,
+				attemptNumber: retryCount,
+				createdAt: new Date()
+			};
+			await drizzle.insert(webhookLog).values(logToInsert);
+		} catch (error) {
+			log.error({ error, webhook, payload, organizationId }, 'Failed to insert webhook log');
+		}
+	}
+}
+
+export async function triggerWebhook({
+	payload,
+	organizationId
+}: {
+	payload: WebhookPayload;
+	organizationId: string;
 }) {
 	try {
 		log.info(
 			{
 				payloadType: payload.type,
 				organizationId,
-				timestamp: new Date().toISOString(),
-				retryDelay,
-				retryCount
+				timestamp: new Date().toISOString()
 			},
 			'Triggering webhook'
 		);
@@ -60,117 +184,16 @@ export async function triggerWebhook({
 				)
 		});
 		log.info({ webhooksCount: returnedWebhooks.length }, 'Found webhooks to trigger');
+		const queue = await getQueue();
 		for (const webhook of returnedWebhooks) {
-			const webhookBody = {
-				id: uuidv7(),
-				type: eventType,
-				createdAt: new Date().toISOString(),
-				apiVersion: CURRENT_API_VERSION,
-				data: parsed.data
-			};
-			let responseText: string = 'NOT_SENT';
-			let httpStatusCode: number = 418;
-			let responseOk: boolean = false;
-			const controller = new AbortController();
-			const timeoutId = setTimeout(() => controller.abort(), 15000); // 15s timeout
-			try {
-				const response = await fetch(webhook.targetUrl, {
-					method: 'POST',
-					signal: controller.signal,
-					headers: {
-						'Content-Type': 'application/json',
-						'X-Webhook-Secret': webhook.secret
-					},
-					body: JSON.stringify(webhookBody)
-				});
-
-				responseText = await response.text();
-				httpStatusCode = response.status;
-				responseOk = response.ok;
-				if (responseOk) {
-					await drizzle
-						.update(webhookTable)
-						.set({
-							lastSuccessAt: new Date()
-						})
-						.where(eq(webhookTable.id, webhook.id));
-				} else {
-					throw new Error(
-						`Webhook failed [${response.status} ${response.statusText}]: ${responseText}`
-					);
-				}
-			} catch (error) {
-				log.error(
-					{
-						status: `${httpStatusCode} ${responseText}`,
-						webhookId: webhook.id,
-						error,
-						type: eventType,
-						payload: parsed.data,
-						organizationId,
-						retryDelay,
-						retryCount
-					},
-					'Failed to trigger webhook'
-				);
-				await drizzle
-					.update(webhookTable)
-					.set({
-						lastFailureAt: new Date()
-					})
-					.where(eq(webhookTable.id, webhook.id));
-
-				if (retryCount < MAX_RETRY_COUNT) {
-					const queue = await getQueue();
-					const newRetryDelay = getRetryDelay(retryDelay, retryCount);
-					log.info(
-						{
-							webhookId: webhook.id,
-							eventType,
-							payload,
-							organizationId,
-							retryDelay,
-							retryCount,
-							newRetryDelay
-						},
-						'Retrying webhook'
-					);
-					await queue.triggerWebhook(
-						{
-							organizationId,
-							payload,
-							retryDelay: newRetryDelay,
-							retryCount: retryCount + 1
-						},
-						{
-							startAfter: newRetryDelay //in seconds
-						}
-					);
-				} else {
-					log.error(
-						{ webhookId: webhook.id, eventType, payload, organizationId, retryDelay, retryCount },
-						'CRITICAL: Webhook failed after max retries'
-					);
-				}
-			} finally {
-				clearTimeout(timeoutId);
-				try {
-					const logToInsert: typeof webhookLog.$inferInsert = {
-						id: uuidv7(),
-						webhookId: webhook.id,
-						eventType: eventType,
-						status: responseOk ? 'success' : 'failed',
-						payload: webhookBody,
-						httpStatusCode: httpStatusCode,
-						responseBody: responseText,
-						attemptNumber: 1,
-						createdAt: new Date()
-					};
-					await drizzle.insert(webhookLog).values(logToInsert);
-				} catch (error) {
-					log.error({ error, webhook, payload, organizationId }, 'Failed to insert webhook log');
-				}
-			}
+			await queue.sendWebhook({
+				webhook,
+				eventType,
+				payload,
+				organizationId,
+				retryDelay: 0,
+				retryCount: 0
+			});
 		}
 	} catch (error) {
 		log.error({ error, payload, organizationId }, 'Failed to trigger webhook');
