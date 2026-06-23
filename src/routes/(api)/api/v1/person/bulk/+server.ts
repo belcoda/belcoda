f655f77a4184df2729fd93a7f7f1bce6015ca7c9
Django Merope <dmerope@gmail.com>
2026-06-23 23:43:06 +0900

    Simple route handler for api bulk upsert queueing

---
+++ src/routes/(api)/api/v1/person/bulk/+server.ts
@@ -0,0 +1,16 @@
+import { json, error } from '@sveltejs/kit';
+import {
+	safeApiRouteQueryContext,
+	processIncomingBody,
+	processOutgoingBody
+} from '$lib/server/utils/restApi';
+import { bulkUpsertPersonRestSchema } from '$lib/schema/person';
+import { getQueue } from '$lib/server/queue/index.js';
+
+export async function PUT(event) {
+	const { organizationId, ctx } = safeApiRouteQueryContext(event.locals.authorizedApiOrganization);
+	const input = await processIncomingBody(event, bulkUpsertPersonRestSchema);
+	const queue = await getQueue();
+	await queue.apiBulkUpsert({ organizationId, csvUrl: input.csvUrl });
+	return json({ queued: true }, { status: 202 });
+}
