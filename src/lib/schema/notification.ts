23b8ec6dd85abd9e7900de698d402476d191da7b
Kenneth Lebu <kenlebu@gmail.com>
2026-06-29 22:03:15 +0300

    refractor types

--- src/lib/schema/notification.ts
+++ src/lib/schema/notification.ts
@@ -1,13 +1,10 @@
 import * as v from 'valibot';
 import * as helpers from '$lib/schema/helpers';
 
-export const notificationPayloadSchema = v.object({
-	personName: v.optional(v.nullable(v.string())),
-	personId: v.optional(v.nullable(helpers.uuid)),
-	subjectTitle: v.optional(v.nullable(v.string())),
-	message: v.optional(v.nullable(v.string()))
-});
-export type NotificationPayload = v.InferOutput<typeof notificationPayloadSchema>;
+export {
+	notificationPayloadSchema,
+	type NotificationPayload
+} from '$lib/schema/notification/payload';
 
 export const notificationStatuses = ['unread', 'read', 'dismissed'] as const;
 export const notificationStatus = v.picklist(notificationStatuses);
