be3117a5dd01d11ce088de56308ed0db067257e9
Kenneth Lebu <kenlebu@gmail.com>
2026-06-29 22:00:30 +0300

    payload schema

--- src/lib/schema/notification.ts
+++ src/lib/schema/notification.ts
@@ -1,6 +1,14 @@
 import * as v from 'valibot';
 import * as helpers from '$lib/schema/helpers';
 
+export const notificationPayloadSchema = v.object({
+	personName: v.optional(v.nullable(v.string())),
+	personId: v.optional(v.nullable(helpers.uuid)),
+	subjectTitle: v.optional(v.nullable(v.string())),
+	message: v.optional(v.nullable(v.string()))
+});
+export type NotificationPayload = v.InferOutput<typeof notificationPayloadSchema>;
+
 export const notificationStatuses = ['unread', 'read', 'dismissed'] as const;
 export const notificationStatus = v.picklist(notificationStatuses);
 export type NotificationStatus = v.InferOutput<typeof notificationStatus>;
