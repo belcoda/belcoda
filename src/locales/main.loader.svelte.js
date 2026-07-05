bafa3deeb3c71855be2340bb00ccd6c22d4f63b0
Django Merope <dmerope@gmail.com>
2026-07-05 23:47:32 +0900

    Locales

--- src/locales/main.loader.svelte.js
+++ src/locales/main.loader.svelte.js
@@ -1,13 +1,12 @@
 import { defaultCollection, registerLoaders } from 'wuchale/load-utils'
-import { loadCatalog, loadCount } from './.wuchale/main.proxy.js'
+import { loadCatalog, loadIDs } from './.wuchale/main.proxy.js'
 
 const key = 'main'
 
-/** @type import('wuchale/runtime').Runtime[] */
-const runtimes = $state([])
+const runtimes = $state({})
 
 // for non-reactive
-export const getRuntime = registerLoaders(key, loadCatalog, loadCount, defaultCollection(runtimes))
+export const getRuntime = registerLoaders(key, loadCatalog, loadIDs, defaultCollection(runtimes))
 
 // same function, only will be inside $derived when used
 export const getRuntimeRx = getRuntime
