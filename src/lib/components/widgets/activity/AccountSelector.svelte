a1e265f514441d3b0c4a79f799298c10775e2312
Django Merope <dmerope@gmail.com>
2026-07-16 08:05:12 +0900

    Draft account selector component

---
+++ src/lib/components/widgets/activity/AccountSelector.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import * as Tabs from '$lib/components/ui/tabs';
+
+	let { selectedAccountId = $bindable() }: { selectedAccountId: string } = $props();
+</script>
+
+<Tabs.Root class="w-full" bind:value={selectedAccountId}>
+	<Tabs.List class="w-full border-b rounded-none">
+		<Tabs.Trigger value="all">Activity</Tabs.Trigger>
+		<Tabs.Trigger value="comments">Comments</Tabs.Trigger>
+	</Tabs.List>
+</Tabs.Root>
