bd7e7fb724ca7c1bca1c96ece6d73e499cd01f72
Kenneth Lebu <kenlebu@gmail.com>
2026-07-17 18:15:28 +0300

    default production log level to info

--- src/lib/pino.ts
+++ src/lib/pino.ts
@@ -1,6 +1,7 @@
+import { dev } from '$app/environment';
 import pino from 'pino';
 
-const logger = pino({ level: 'debug' });
+const logger = pino({ level: dev ? 'debug' : 'info' });
 
 export default function createChildLogger(file: string) {
 	const child = logger.child({ file });
