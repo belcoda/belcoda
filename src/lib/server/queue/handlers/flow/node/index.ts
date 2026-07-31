1e79e94aeb88c01c8a2a14abfcc75834050c6489
Django Merope <dmerope@gmail.com>
2026-07-31 09:24:43 +0900

    Node handler logic scaffolding

--- src/lib/server/queue/handlers/flow/node/index.ts
+++ src/lib/server/queue/handlers/flow/node/index.ts
@@ -1,20 +1,40 @@
-import { type Flow } from '$lib/schema/flow/node/index.js';
+import { db } from '$lib/server/db';
+import { createFlowDefinitionChecksum } from '$lib/server/api/data/flows/document.js';
+import { _getFlowVersionUnsafe } from '$lib/server/api/data/flows/version';
+import { _getFlowExecutionUnsafe } from '$lib/server/api/data/flows/execution';
 import {
-	person as personTable,
-	flowExecution as flowExecutionTable,
-	flowVersion as flowVersionTable
-} from '$lib/schema/drizzle.js';
+	_createFlowExecutionStep,
+	_updateFlowExecutionStep
+} from '$lib/server/api/data/flows/execution_step';
 export async function processFlowNode({
-	flowVersion,
-	person,
-	flowExecution,
+	flowVersionId,
+	personId,
+	organizationId,
+	flowExecutionId,
 	nodeId
 }: {
-	flowVersion: typeof flowVersionTable.$inferSelect;
-	person: typeof personTable.$inferSelect;
-	flowExecution: typeof flowExecutionTable.$inferSelect;
+	flowVersionId: string;
+	personId: string;
+	organizationId: string;
+	flowExecutionId: string;
 	nodeId: string;
 }): Promise<void> {
+	const [flowVersion, flowExecution] = await db.transaction(async (tx) => {
+		const [flowVersion, flowExecution] = await Promise.all([
+			_getFlowVersionUnsafe({
+				tx,
+				organizationId,
+				flowVersionId
+			}),
+			_getFlowExecutionUnsafe({
+				tx,
+				organizationId,
+				personId,
+				flowExecutionId
+			})
+		]);
+		return [flowVersion, flowExecution];
+	});
 	const node = flowVersion.flowDefinition.nodes.find((node) => node.id === nodeId);
 	if (flowVersion.checksum !== createFlowDefinitionChecksum(flowVersion.flowDefinition)) {
 		throw new Error(
@@ -26,8 +46,36 @@ export async function processFlowNode({
 			`Node ${nodeId} not found in flow ${flowVersion.flowDefinition} for execution ${flowExecution.id}`
 		);
 	}
+	//1 create an step execution record in the database
+	const flowExecutionStep = await db.transaction(async (tx) => {
+		return await _createFlowExecutionStep({
+			tx,
+			flowExecutionId,
+			nodeId,
+			status: 'pending',
+			attemptNumber: 1
+		});
+	});
 
-	switch (node.data.type) {
-		default:
+	try {
+		// the bulk of the logic of the handlers should sit in here...
+		switch (node.data.type) {
+			default: {
+				throw new Error(`Unknown node type: ${node.data.type}`);
+			}
+		}
+	} catch (error) {
+		let errorMessage = '';
+		if (error instanceof Error) {
+			errorMessage = error.message;
+		}
+		await db.transaction(async (tx) => {
+			await _updateFlowExecutionStep({
+				tx,
+				flowExecutionStepId: flowExecutionStep.id,
+				status: 'failed',
+				error: { message: errorMessage }
+			});
+		});
 	}
 }
