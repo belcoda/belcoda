76192d46a37cc7b907302eece08ef0b9b3e33829
Django Merope <dmerope@gmail.com>
2026-07-31 13:24:44 +0900

    Trigger event signup & updating typing

--- src/lib/server/queue/handlers/flow/node/index.ts
+++ src/lib/server/queue/handlers/flow/node/index.ts
@@ -1,51 +1,38 @@
 import { db } from '$lib/server/db';
-import { createFlowDefinitionChecksum } from '$lib/server/api/data/flows/document.js';
-import { _getFlowVersionUnsafe } from '$lib/server/api/data/flows/version';
-import { _getFlowExecutionUnsafe } from '$lib/server/api/data/flows/execution';
+import { type Flow } from '$lib/schema/flow/node/index';
+import { _getFlowDetailsUnsafe } from '$lib/server/api/data/flows/utils';
 import {
 	_createFlowExecutionStep,
 	_updateFlowExecutionStep
 } from '$lib/server/api/data/flows/execution_step';
+import { processFlowNodeEventSignup } from './eventSignup';
+
+export type ProcessFlowNodeProps = {
+	flowVersionId: string;
+	personId: string;
+	organizationId: string;
+	flowExecutionId: string;
+	nodeId: string;
+};
+
+export type NodeHandlerProps = ProcessFlowNodeProps & {
+	flowExecutionStepId: string;
+};
 export async function processFlowNode({
 	flowVersionId,
 	personId,
 	organizationId,
 	flowExecutionId,
 	nodeId
-}: {
-	flowVersionId: string;
-	personId: string;
-	organizationId: string;
-	flowExecutionId: string;
-	nodeId: string;
-}): Promise<void> {
-	const [flowVersion, flowExecution] = await db.transaction(async (tx) => {
-		const [flowVersion, flowExecution] = await Promise.all([
-			_getFlowVersionUnsafe({
-				tx,
-				organizationId,
-				flowVersionId
-			}),
-			_getFlowExecutionUnsafe({
-				tx,
-				organizationId,
-				personId,
-				flowExecutionId
-			})
-		]);
-		return [flowVersion, flowExecution];
+}: ProcessFlowNodeProps): Promise<void> {
+	const { node } = await _getFlowDetailsUnsafe({
+		flowExecutionId,
+		personId,
+		nodeId,
+		organizationId,
+		flowVersionId
 	});
-	const node = flowVersion.flowDefinition.nodes.find((node) => node.id === nodeId);
-	if (flowVersion.checksum !== createFlowDefinitionChecksum(flowVersion.flowDefinition)) {
-		throw new Error(
-			`Flow version ${flowVersion.id} has a different checksum than the flow definition`
-		);
-	}
-	if (!node) {
-		throw new Error(
-			`Node ${nodeId} not found in flow ${flowVersion.flowDefinition} for execution ${flowExecution.id}`
-		);
-	}
+
 	//1 create an step execution record in the database
 	const flowExecutionStep = await db.transaction(async (tx) => {
 		return await _createFlowExecutionStep({
@@ -60,6 +47,17 @@ export async function processFlowNode({
 	try {
 		// the bulk of the logic of the handlers should sit in here...
 		switch (node.data.type) {
+			case 'event.signup': {
+				await processFlowNodeEventSignup({
+					flowVersionId,
+					personId,
+					organizationId,
+					flowExecutionId,
+					flowExecutionStepId: flowExecutionStep.id,
+					nodeId
+				});
+				break;
+			}
 			default: {
 				throw new Error(`Unknown node type: ${node.data.type}`);
 			}
@@ -77,5 +75,36 @@ export async function processFlowNode({
 				error: { message: errorMessage }
 			});
 		});
+	}
+}
+
+// optional handleId is for nodes with multiple handles and multiple outgoing edges
+export function getNextNodeToProcess({
+	nodeId,
+	handleId,
+	flow
+}: {
+	nodeId: string;
+	handleId?: string;
+	flow: Flow;
+}): string | null {
+	const node = flow.nodes.find((node) => node.id === nodeId);
+	if (!node) {
+		throw new Error(`Node not found: ${nodeId}`);
+	}
+	if (handleId) {
+		const edge = flow.edges.find(
+			(edge) => edge.source === node.id && edge.sourceHandle === handleId
+		);
+		if (!edge) {
+			return null;
+		}
+		return edge.target;
+	} else {
+		const nextNodeSource = flow.edges.find((edge) => edge.source === node.id);
+		if (!nextNodeSource) {
+			return null;
+		}
+		return nextNodeSource.target;
 	}
 }
