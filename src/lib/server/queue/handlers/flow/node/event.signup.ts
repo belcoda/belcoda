c6b07441885d7dc4e1a2bf2a994ac8e654575aa1
Django Merope <dmerope@gmail.com>
2026-08-01 01:13:40 +0900

    Proper function name

--- src/lib/server/queue/handlers/flow/node/event.signup.ts
+++ src/lib/server/queue/handlers/flow/node/event.signup.ts
@@ -63,7 +63,7 @@ export async function processFlowNodeEventSignup({
 					flowExecutionId,
 					nodeId: nextNodeToProcess
 				},
-				getQueueSendOptionsFromTransaction(tx)
+				queueSendOptionsFromTransaction(tx)
 			);
 		}
 	});
