f3714aafac381eed5c8d90b4f0f089923b2803c0
Django Merope <dmerope@gmail.com>
2026-08-07 17:02:31 +0900

    Stub functions for linked device API

---
+++ src/lib/server/utils/whatsapp/linkeddevice/convert_outbound.ts
@@ -0,0 +1,7 @@
+import { type WhatsappMessage } from '$lib/schema/whatsapp/message';
+
+// stub function for converting outbound messages to Titan API format
+export function convertOutboundMessage({ message }: { message: WhatsappMessage }) {
+	//convert message to outbound message...
+	return message;
+}
