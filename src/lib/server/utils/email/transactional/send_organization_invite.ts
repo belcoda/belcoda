2d4d5608989d9ef392f980e45f600aaebbcd257c
Django Merope <dmerope@gmail.com>
2026-08-17 21:02:07 +0900

    Function to send an organizational invitation email
    Can be copied for other types of email

---
+++ src/lib/server/utils/email/transactional/send_organization_invite.ts
@@ -0,0 +1,47 @@
+import sendHtmlEmail from '$lib/server/utils/email/send_html_email';
+import { type Locale } from '$lib/utils/language';
+import { Renderer } from '@better-svelte-email/server';
+const renderer = new Renderer();
+import SingleButtonTransactional from '$lib/emails/templates/SingleButtonTransactional.svelte';
+import type { Props as SingleButtonTransactionalProps } from '$lib/emails/templates/SingleButtonTransactional.svelte';
+import { t } from '$lib/index.svelte';
+
+export async function sendOrganizationInvitationEmail({
+	url,
+	orgIcon,
+	emailAddress,
+	inviterName,
+	organizationName,
+	locale
+}: {
+	url: string;
+	emailAddress: string;
+	orgIcon?: string | null;
+	inviterName: string;
+	organizationName: string;
+	locale: Locale;
+}) {
+	const body = t`${inviterName} has invited you to join ${organizationName} on Belcoda. Click the button below to accept the invitation and get started.`;
+	const props: SingleButtonTransactionalProps = {
+		language: locale,
+		instanceUrl: url,
+		title: t`Join ${organizationName}`,
+		body: body,
+		logoUrl: orgIcon || undefined,
+		previewText: t`${inviterName} has invited you to join ${organizationName} on Belcoda`,
+		buttonText: t`Accept invitation`,
+		buttonUrl: url,
+		instanceName: organizationName,
+		logoAlt: organizationName,
+		buttonAltHtml: t`Copy and paste the following link into your browser ${url}`,
+		copyright: t`Copyright ${new Date().getFullYear().toString()} ${organizationName}`
+	};
+	const html = await renderer.render(SingleButtonTransactional, { props });
+	await sendHtmlEmail({
+		to: emailAddress,
+		from: `${organizationName} <noreply@belcoda.com>`,
+		html,
+		subject: `Invitation to join ${organizationName}`,
+		stream: 'outbound'
+	});
+}
