1e2d9835eca62d904e2fc67bb7f17b10aef4c031
Django Merope <dmerope@gmail.com>
2026-08-17 22:59:19 +0900

    Run with locale

--- src/lib/server/utils/email/transactional/send_organization_invite.ts
+++ src/lib/server/utils/email/transactional/send_organization_invite.ts
@@ -5,6 +5,15 @@ const renderer = new Renderer();
 import SingleButtonTransactional from '$lib/emails/templates/SingleButtonTransactional.svelte';
 import type { Props as SingleButtonTransactionalProps } from '$lib/emails/templates/SingleButtonTransactional.svelte';
 import { t } from '$lib/index.svelte';
+import { runWithLocale } from 'wuchale/load-utils/server';
+
+function formatEmailDisplayName(name: string): string {
+	const cleaned = name.replace(/[\r\n\t]+/g, ' ').trim();
+	if (/[()<>@,;:\\".[\]]/.test(cleaned)) {
+		return `"${cleaned.replace(/(["\\])/g, '\\$1')}"`;
+	}
+	return cleaned;
+}
 
 export async function sendOrganizationInvitationEmail({
 	url,
@@ -21,27 +30,29 @@ export async function sendOrganizationInvitationEmail({
 	organizationName: string;
 	locale: Locale;
 }) {
-	const body = t`${inviterName} has invited you to join ${organizationName} on Belcoda. Click the button below to accept the invitation and get started.`;
-	const props: SingleButtonTransactionalProps = {
-		language: locale,
-		instanceUrl: url,
-		title: t`Join ${organizationName}`,
-		body: body,
-		logoUrl: orgIcon || undefined,
-		previewText: t`${inviterName} has invited you to join ${organizationName} on Belcoda`,
-		buttonText: t`Accept invitation`,
-		buttonUrl: url,
-		instanceName: organizationName,
-		logoAlt: organizationName,
-		buttonAltHtml: t`Copy and paste the following link into your browser ${url}`,
-		copyright: t`Copyright ${new Date().getFullYear().toString()} ${organizationName}`
-	};
-	const html = await renderer.render(SingleButtonTransactional, { props });
-	await sendHtmlEmail({
-		to: emailAddress,
-		from: `${organizationName} <noreply@belcoda.com>`,
-		html,
-		subject: `Invitation to join ${organizationName}`,
-		stream: 'outbound'
+	return await runWithLocale(locale, async () => {
+		const body = t`${inviterName} has invited you to join ${organizationName} on Belcoda. Click the button below to accept the invitation and get started.`;
+		const props: SingleButtonTransactionalProps = {
+			language: locale,
+			instanceUrl: url,
+			title: t`Join ${organizationName}`,
+			body: body,
+			logoUrl: orgIcon || undefined,
+			previewText: t`${inviterName} has invited you to join ${organizationName} on Belcoda`,
+			buttonText: t`Accept invitation`,
+			buttonUrl: url,
+			instanceName: organizationName,
+			logoAlt: orgIcon ? organizationName : t`Belcoda logo`,
+			buttonAltHtml: t`Copy and paste the following link into your browser ${url}`,
+			copyright: t`Copyright ${new Date().getFullYear().toString()} ${organizationName}`
+		};
+		const html = await renderer.render(SingleButtonTransactional, { props });
+		await sendHtmlEmail({
+			to: emailAddress,
+			from: `${formatEmailDisplayName(organizationName)} <noreply@belcoda.com>`,
+			html,
+			subject: t`Invitation to join ${organizationName}`,
+			stream: 'outbound'
+		});
 	});
 }
