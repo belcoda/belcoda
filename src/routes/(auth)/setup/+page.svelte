470f75a59662bbe75480e2ffd4e07e65b32f672d
Kenneth Lebu <kenlebu@gmail.com>
2026-08-18 21:44:14 +0300

    create setup page at /setup and add the components to it

---
+++ src/routes/(auth)/setup/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import SetupPage from '$lib/components/widgets/organization-onboarding/SetupPage.svelte';
+</script>
+
+<svelte:head>
+	<title>Set up your organization</title>
+</svelte:head>
+
+<SetupPage oninvite={() => console.log('open invite drawer')} />
