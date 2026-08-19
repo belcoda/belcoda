a5c0380b0088a71af2ff5317466b92b754e60680
Kenneth Lebu <kenlebu@gmail.com>
2026-08-19 16:49:55 +0300

    component for whatsapp connection. This has alternatives and doesn't absolutely need to exist. the commit can be reverted if there's need to

---
+++ src/routes/(auth)/setup/whatsapp/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import ConnectWhatsAppSetup from '$lib/components/widgets/organization-onboarding/ConnectWhatsAppSetup.svelte';
+</script>
+
+<svelte:head>
+	<title>Connect WhatsApp</title>
+</svelte:head>
+
+<ConnectWhatsAppSetup />
