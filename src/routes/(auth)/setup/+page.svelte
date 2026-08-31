9c74c1b3fce6cd43e67452835622bebbcd998662
Kenneth Lebu <kenlebu@gmail.com>
2026-08-31 16:05:55 +0300

    open invite drawer from /setup instead of logging

--- src/routes/(auth)/setup/+page.svelte
+++ src/routes/(auth)/setup/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import SetupPage from '$lib/components/widgets/organization-onboarding/SetupPage.svelte';
+	import InviteTeammatesDrawer from '$lib/components/widgets/organization-onboarding/InviteTeammatesDrawer.svelte';
+
+	let inviteOpen = $state(false);
 </script>
 
 <svelte:head>
 	<title>Set up your organization</title>
 </svelte:head>
 
-<SetupPage oninvite={() => console.log('open invite drawer')} />
+<SetupPage oninvite={() => (inviteOpen = true)} />
+<InviteTeammatesDrawer bind:open={inviteOpen} />
