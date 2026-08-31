23d2ae609e4197459225f1cc51390e6cfcc64202
Django Merope <dmerope@gmail.com>
2026-08-31 17:11:44 +0900

    Flow components for updated flow management system

---
+++ src/lib/components/flow2/Flow.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import '@xyflow/svelte/dist/style.css';
+	import { SvelteFlowProvider, type Node, type Edge } from '@xyflow/svelte';
+	const {
+		backButtonUrl,
+		disabled = false,
+		loadFlowFunction
+	}: {
+		backButtonUrl?: string;
+		disabled?: boolean;
+		loadFlowFunction: () => Promise<{ nodes: Node[]; edges: Edge[] }>;
+	} = $props();
+
+	import FlowStateManager from '$lib/components/flow2/StateManager.svelte';
+	import FlowCanvas from '$lib/components/flow2/FlowCanvas.svelte';
+</script>
+
+<div class="h-full w-full">
+	<SvelteFlowProvider>
+		<FlowStateManager {loadFlowFunction} />
+		<FlowCanvas {disabled} />
+	</SvelteFlowProvider>
+</div>
