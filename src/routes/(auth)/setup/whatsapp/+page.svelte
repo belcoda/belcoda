49477b198ec7cda70c450e3c71719c8c0ea7230b
Kenneth Lebu <kenlebu@gmail.com>
2026-08-31 16:02:54 +0300

    localize setup and whatsapp page title

--- src/routes/(auth)/setup/whatsapp/+page.svelte
+++ src/routes/(auth)/setup/whatsapp/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import { t } from '$lib/index.svelte';
 	import ConnectWhatsAppSetup from '$lib/components/widgets/organization-onboarding/ConnectWhatsAppSetup.svelte';
 </script>
 
 <svelte:head>
-	<title>Connect WhatsApp</title>
+	<title>{t`Connect WhatsApp`}</title>
 </svelte:head>
 
 <ConnectWhatsAppSetup />
