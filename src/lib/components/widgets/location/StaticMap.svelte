0dccccb31668dc0da9dc0bd590f87376a0beba88
Django Merope <dmerope@gmail.com>
2026-09-01 23:50:34 +0900

    Map components

---
+++ src/lib/components/widgets/location/StaticMap.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { env } from '$env/dynamic/public';
+	import { cn } from '$lib/utils.js';
+	let {
+		latitude,
+		longitude,
+		class: className
+	}: { latitude: number; longitude: number; class?: string } = $props();
+</script>
+
+<img
+	class={cn('bg-background/60 rounded-md w-auto', className)}
+	src="https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/pin-s+2b7fff({longitude},{latitude})/{longitude},{latitude},14,0/600x400.png?attribution=false&logo=false&access_token={env.PUBLIC_MAPBOX_TOKEN}"
+	alt="Map of {latitude}, {longitude}"
+/>
